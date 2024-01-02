a1382286ee58726e68bbc186e888b5b1036cc7d1
Gaoridang <oingc@naver.com>
2024-01-02 17:32:35 +0900

    Feat: 기본 라우팅 추가

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,5 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
 
-const router = createBrowserRouter([]);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <h1>Hello World</h1>
+  }
+]);
 
 export default router;
