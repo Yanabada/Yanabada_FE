e1c0af28fa57e4c15a1dbe86d2869b9e6036042d
고솔미 (Solmi Koh) <113079102+SOL-MI@users.noreply.github.com>
2024-01-04 16:42:10 +0900

    Design : 하단 NavBar 레이아웃

---
+++ src/components/navBar/index.tsx
@@ -0,0 +1,72 @@
+// import React from "react";
+// import navChat from "../../assets/icons/nav_Chat.svg";
+// import navAll from "../../assets/icons/nav_All.svg";
+// import navHome from "../../assets/icons/nav_Home.svg";
+// import navProduct from "../../assets/icons/nav_Product.svg";
+// import navMy from "../../assets/icons/nav_My.svg";
+
+const NavBar = () => {
+  // const basicBoxStyles = {
+  //   flex: "1",
+  //   flexDir: "column",
+  //   alignItems: "center",
+  //   fontSize: "12px",
+  //   gap: "5px"
+  // };
+
+  return (
+    <div>안녕~</div>
+    // <Flex
+    //   justify="space-between"
+    //   alignItems="center"
+    //   bg="primary.white"
+    //   w="100%"
+    //   h="56px"
+    //   pos="fixed"
+    //   bottom="0"
+    //   left="0"
+    //   right="0"
+    //   textAlign="center"
+    // >
+    //   <Button variant={{ base: "base", md: "md" }}>Object syntax</Button>
+    //   <Flex sx={basicBoxStyles}>
+    //     <img src={navHome} />홈
+    //   </Flex>
+    //   <Flex sx={basicBoxStyles}>
+    //     <img src={navAll} />
+    //     상품리스트
+    //   </Flex>
+    //   <Flex sx={basicBoxStyles}>
+    //     <img src={navProduct} />
+    //     등록
+    //   </Flex>
+    //   <Flex sx={basicBoxStyles}>
+    //     <Avatar src={navChat} size="22px" borderRadius="0">
+    //       <AvatarBadge
+    //         placement="top-end"
+    //         transform="translate(35%, -40%)"
+    //         boxSize="1.25em"
+    //         fontSize="12px"
+    //         border="none"
+    //         bg="primary.100"
+    //         alignItems="baseline"
+    //       >
+    //         2
+    //       </AvatarBadge>
+    //     </Avatar>
+    //     채팅
+    //   </Flex>
+    //   {/* <Avatar src={navChat}>
+    //     <AvatarBadge placement="top-end" boxSize="1.25em" bg="primary.100">
+    //       3
+    //     </AvatarBadge>
+    //   </Avatar> */}
+    //   <Flex sx={basicBoxStyles}>
+    //     <img src={navMy} />
+    //     마이
+    //   </Flex>
+    // </Flex>
+  );
+};
+
+export default NavBar;
--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,9 +1,10 @@
+import NavBar from "components/navBar";
 import { createBrowserRouter } from "react-router-dom";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <h1>Hello World</h1>
+    element: <NavBar />
   }
 ]);
 
