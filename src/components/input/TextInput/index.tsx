909ec2e01c0423797c2864a454683f4aa7becbde
Gaoridang <oingc@naver.com>
2024-01-05 15:22:38 +0900

    Feat: 인풋 컴포넌트 forwardRef 추가

--- src/components/input/TextInput/index.tsx
+++ src/components/input/TextInput/index.tsx
@@ -1,37 +1,35 @@
-import { ComponentProps, ReactNode } from "react";
+import { ComponentProps, ReactNode, forwardRef } from "react";
 import { IoCheckmark } from "react-icons/io5";
 import * as S from "./styles";
 
 type Picked = "onChange" | "placeholder" | "value" | "type";
 
 interface InputProps extends Pick<ComponentProps<"input">, Picked> {
-  variant?: "static" | "smooth";
+  variant?: "static" | "move";
   label?: string;
   errorMessage?: string;
   isSuccess?: boolean;
   rightElement?: ReactNode;
 }
 
-const TextInput = ({
-  variant = "smooth",
-  errorMessage,
-  isSuccess = false,
-  label,
-  rightElement = null,
-  ...props
-}: InputProps) => {
-  return (
-    <>
-      {variant === "static" && <S.Label>{label}</S.Label>}
-      <S.InputWrapper>
-        <S.Input required className={`${errorMessage ? "error" : ""}`} {...props} />
-        {variant === "smooth" && <S.Label className="move">{label}</S.Label>}
-        {rightElement && <S.RightElement>{rightElement}</S.RightElement>}
-        {isSuccess && <S.RightElement>{IoCheckmark}</S.RightElement>}
-      </S.InputWrapper>
-      {errorMessage && <S.ErrorMessage>{errorMessage}</S.ErrorMessage>}
-    </>
-  );
-};
+const TextInput = forwardRef<HTMLInputElement, InputProps>(
+  (
+    { variant = "move", errorMessage, isSuccess = false, label, rightElement = null, ...props },
+    ref
+  ) => {
+    return (
+      <>
+        {variant === "static" && <S.Label>{label}</S.Label>}
+        <S.InputWrapper>
+          <S.Input ref={ref} required className={`${errorMessage ? "error" : ""}`} {...props} />
+          {variant === "move" && <S.Label className="move">{label}</S.Label>}
+          {rightElement && <S.RightElement>{rightElement}</S.RightElement>}
+          {isSuccess && <S.RightElement>{IoCheckmark}</S.RightElement>}
+        </S.InputWrapper>
+        {errorMessage && <S.ErrorMessage>{errorMessage}</S.ErrorMessage>}
+      </>
+    );
+  }
+);
 
 export default TextInput;
