b8438a7e794c291e144c8e74adb831c1dd2c8a14
seungsimdang <oak20005@naver.com>
2024-01-06 01:02:47 +0900

    Design: AuthenticationButton 디자인

---
+++ src/components/buttons/AuthenticationButton/index.tsx
@@ -0,0 +1,23 @@
+import * as S from "./styles";
+
+interface AuthenticationButtonProps
+  extends Omit<React.ButtonHTMLAttributes<HTMLButtonElement>, "type"> {
+  type: "light" | "default" | "abled" | "disabled";
+}
+
+const index = ({ type, children, ...props }: AuthenticationButtonProps) => {
+  switch (type) {
+    case "light":
+      return <S.LightAuthenticationButton {...props}>{children}</S.LightAuthenticationButton>;
+    case "default":
+      return <S.DefaultAuthenticationButton {...props}>{children}</S.DefaultAuthenticationButton>;
+    case "abled":
+      return <S.AbledAuthenticationButton {...props}>{children}</S.AbledAuthenticationButton>;
+    case "disabled":
+      return <S.DisAbledAuthenticationButton {...props}>{children}</S.DisAbledAuthenticationButton>;
+    default:
+      return null;
+  }
+};
+
+export default index;
