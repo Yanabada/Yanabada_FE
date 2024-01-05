d8323b6930770ece1cad6f57426e050859a71198
seungsimdang <oak20005@naver.com>
2024-01-05 23:14:39 +0900

    Fix: 아이콘 크기 적용 안되는 오류 해결

--- src/components/buttons/CardSectionButton/index.tsx
+++ src/components/buttons/CardSectionButton/index.tsx
@@ -10,24 +10,17 @@ const CardSectionButton = () => {
           <S.ListButtonText>야놀자 페이</S.ListButtonText>
           <S.RightSectionWrapper>
             <S.RightSectionText>0원</S.RightSectionText>
-            <IoIosArrowForward
-              display="flex"
-              width="22px"
-              height="22px"
-              justify-content="center"
-              align-items="center"
-              color="#616161"
-            />
+            <IoIosArrowForward size="18px" color="#616161" />
           </S.RightSectionWrapper>
         </S.ListButton>
       </S.ListButtonWrapper>
       <S.BottomSectionWrapper>
         <S.BottomLeftButton>
-          <IoIosSearch width="18px" height="18px" color="#0751C3" />
+          <IoIosSearch size="18px" color="#0751C3" />
           <S.BottomLeftButtonText>충전</S.BottomLeftButtonText>
         </S.BottomLeftButton>
         <S.BottomRightButton>
-          <IoIosSearch width="18px" height="18px" color="#FFFFFF" />
+          <IoIosSearch size="18px" color="#FFFFFF" />
           <S.BottomRightButtonText>계좌송금</S.BottomRightButtonText>
         </S.BottomRightButton>
       </S.BottomSectionWrapper>
