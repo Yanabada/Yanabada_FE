b9df027e09e0cc33ed36b129ddd361b3efc579e6
장수빈 <jangsubin@jangsubin-ui-MacBookAir.local>
2024-01-05 21:29:04 +0900

    Design: 최근 본 상품 카드 마크업

---
+++ src/components/card/RecentlyRowCard/styles.ts
@@ -0,0 +1,29 @@
+import * as R from "@components/card/RowCard/styles";
+import styled from "@emotion/styled";
+
+export const Wrapper = styled(R.Wrapper)``;
+
+export const Container = styled(R.Container)`
+  width: 18rem;
+  height: 7.25rem;
+`;
+
+export const ImageContainer = styled(R.ImageContainer)``;
+
+export const Image = styled(R.Image)``;
+
+export const InformationContainer = styled.div`
+  padding: 0.5rem;
+`;
+
+export const ProductContainer = styled.div``;
+
+export const ProductName = styled(R.ProductName)``;
+
+export const RoomName = styled(R.RoomName)``;
+
+export const Period = styled(R.Period)``;
+
+export const TimerContainer = styled(R.TimerContainer)``;
+
+export const TimerText = styled(R.TimerText)``;
