ac3117218dbe972cb36caaa82c26a5b6e699eae7
furaha707 <zxcvyein950823@gmail.com>
2024-01-05 16:49:00 +0900

    Feat: 캘린더 라이브러리 커스텀

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,9 +1,14 @@
+import CustomDatePicker from "../components/Calendar/index";
 import { createBrowserRouter } from "react-router-dom";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <h1>Hello World</h1>
+  },
+  {
+    path: "/calendar",
+    element: <CustomDatePicker />
   }
 ]);
 
