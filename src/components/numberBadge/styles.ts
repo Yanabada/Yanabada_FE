e7ac12c437224a282fb2b74e7a36baef3ab4bff3
고솔미 (Solmi Koh) <113079102+SOL-MI@users.noreply.github.com>
2024-01-05 19:54:01 +0900

    Design : 하단 네비바 뱃지 제작

---
+++ src/components/numberBadge/styles.ts
@@ -0,0 +1,15 @@
+import styled from "@emotion/styled";
+
+export const BadgeCircle = styled.div`
+  background-color: ${({ theme }) => theme.colors.orange[200]};
+  color: #fff;
+  padding: 2px 4px;
+  position: absolute;
+  transform: translate(60%, -40%);
+  &.small {
+    border-radius: 50%;
+  }
+  &.big {
+    border-radius: 16px;
+  }
+`;
