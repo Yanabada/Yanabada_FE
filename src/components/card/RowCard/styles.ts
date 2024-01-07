12702c48eb691a0f8c4be521b08469ed2fd56a9b
장수빈 <jangsubin@jangsubin-ui-MacBookAir.local>
2024-01-07 17:16:00 +0900

    Refactor: Card 공통 스타일 파일 생성

--- src/components/card/RowCard/styles.ts
+++ src/components/card/RowCard/styles.ts
@@ -1,17 +1,16 @@
 import styled from "@emotion/styled";
-import * as C from "@components/card/ColumnCard/styles";
+import * as Card from "../card.styles";
 
-export const Wrapper = styled(C.Wrapper)``;
+export const Wrapper = styled(Card.Wrapper)``;
 
-export const Container = styled(C.Container)`
-  display: flex;
+export const Container = styled(Card.Container)`
   width: 16rem;
   height: 10.5rem;
 `;
 
-export const ImageContainer = styled(C.ImageContainer)``;
+export const ImageContainer = styled(Card.ImageContainer)``;
 
-export const DiscountRate = styled(C.DiscountRate)``;
+export const DiscountRate = styled(Card.DiscountRate)``;
 
 export const TimeOut = styled.div`
   display: flex;
@@ -37,52 +36,36 @@ export const TimerOutText = styled.p`
   letter-spacing: -0.3px;
 `;
 
-export const Image = styled(C.Image)`
+export const Image = styled(Card.Image)`
   width: 6.25rem;
   height: 100%;
 `;
 
-export const LocationContainer = styled(C.LocationContainer)`
+export const LocationContainer = styled(Card.LocationContainer)`
   top: 90%;
 `;
 
-export const Location = styled(C.Location)``;
+export const Location = styled(Card.Location)``;
 
-export const InformationContainer = styled.div`
+export const InformationContainer = styled(Card.InformationContainer)`
   padding: 0.5rem;
 `;
 
-export const ProductContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  margin-top: 0.2rem;
-  margin-bottom: 0.6rem;
-`;
+export const ProductContainer = styled(Card.ProductContainer)``;
 
-export const ProductName = styled.p`
-  color: ${({ theme }) => theme.colors.gray[900]};
-  font-size: 0.875rem;
-  font-weight: 500;
-  line-height: 1.25rem;
-`;
+export const ProductName = styled(Card.ProductName)``;
 
-export const RoomName = styled.p`
-  color: ${({ theme }) => theme.colors.gray[600]};
-  font-size: 0.75rem;
-  font-weight: 400;
-  line-height: 1.125rem;
-  letter-spacing: -0.015rem;
-`;
+export const RoomName = styled(Card.RoomName)``;
 
-export const Period = styled(C.Period)``;
+export const Period = styled(Card.Period)``;
 
-export const TimerContainer = styled(C.TimerContainer)`
+export const TimerContainer = styled(Card.TimerContainer)`
   justify-content: flex-end;
 `;
 
-export const TimerText = styled(C.TimerText)``;
+export const TimerText = styled(Card.TimerText)``;
 
-export const PriceContainer = styled(C.PriceContainer)`
+export const PriceContainer = styled(Card.PriceContainer)`
   margin-right: 0;
 
   &.deadLine {
@@ -90,10 +73,10 @@ export const PriceContainer = styled(C.PriceContainer)`
   }
 `;
 
-export const Price = styled(C.Price)``;
+export const Price = styled(Card.Price)``;
 
-export const SellingContainer = styled(C.SellingContainer)``;
+export const SellingContainer = styled(Card.SellingContainer)``;
 
-export const SellingRate = styled(C.SellingRate)``;
+export const SellingRate = styled(Card.SellingRate)``;
 
-export const SellingPrice = styled(C.SellingPrice)``;
+export const SellingPrice = styled(Card.SellingPrice)``;
