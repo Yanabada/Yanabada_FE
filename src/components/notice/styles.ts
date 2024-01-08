c8b101bbf83034598572a7e6757f12393e3ecc25
고솔미 (Solmi Koh) <113079102+SOL-MI@users.noreply.github.com>
2024-01-09 01:55:03 +0900

    Design : 공통컴포넌트 안내문 구현

---
+++ src/components/notice/styles.ts
@@ -0,0 +1,51 @@
+import styled from "@emotion/styled";
+
+export const NoticeWrapper = styled.div`
+  padding: 8px 10px;
+  border-radius: 5px;
+
+  display: flex;
+  flex-shrink: 0;
+  flex-direction: row;
+  gap: 8px;
+
+  > svg {
+    width: 16px;
+  }
+
+  color: ${({ theme }) => theme.colors.gray[700]};
+
+  &.fill {
+    background-color: ${({ theme }) => theme.colors.gray[100]};
+  }
+  &.lineFill {
+    background-color: ${({ theme }) => theme.colors.gray[200]};
+    border: 1px solid ${({ theme }) => theme.colors.gray[300]};
+  }
+  &.line {
+    border: 1px solid ${({ theme }) => theme.colors.gray[300]};
+  }
+  &.yanolja {
+    align-items: center;
+  }
+`;
+
+export const NoticeTitle = styled.div`
+  color: ${({ theme }) => theme.colors.blue[200]};
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 21px;
+`;
+export const TextWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 21px;
+`;
+
+export const IconBox = styled.div`
+  padding: 2px 0 0 0;
+`;
