c987a3a29ab7470d52d9643d86d5de33c906ff0c
furaha707 <zxcvyein950823@gmail.com>
2024-01-08 20:00:31 +0900

    Fix: 폴더이름

--- src/pages/sell/index.tsx
+++ src/pages/sell/index.tsx
@@ -1,4 +1,4 @@
-import Calendar from "@components/calendar";
+import Calendar from "@components/newCalendar";
 import { startOfMonth, endOfMonth, eachDayOfInterval } from "date-fns";
 
 const Sell = () => {
