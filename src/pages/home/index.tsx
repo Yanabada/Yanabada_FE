3d4f163df2c097a151439545a2d37358a5f0e531
장수빈 <jangsubin@jangsubin-ui-MacBookAir.local>
2024-01-08 15:43:54 +0900

    Feat: page > home index 생성

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,15 +1,5 @@
-import RowCard from "@components/card/RowCard";
-import ColumnCard from "@components/card/ColumnCard";
-import RecentlyRowCard from "@components/card/RecentlyRowCard";
-
 const Home = () => {
-  return (
-    <div>
-      <RecentlyRowCard />
-      <RowCard props="마감임박" />
-      <ColumnCard />
-    </div>
-  );
+  return <div>Home</div>;
 };
 
 export default Home;
