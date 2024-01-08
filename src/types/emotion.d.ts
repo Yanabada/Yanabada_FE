d7b04cf87b5b1e79e1042c68093c25b3a1ac0062
고솔미 (Solmi Koh) <113079102+SOL-MI@users.noreply.github.com>
2024-01-08 15:04:13 +0900

    Refactor : theme shadow type 수정

--- src/types/emotion.d.ts
+++ src/types/emotion.d.ts
@@ -2,13 +2,11 @@ import "@emotion/react";
 import theme from "themes/theme";
 
 type ColorKeys = keyof (typeof theme)["colors"];
+type ShadowKeys = keyof (typeof theme)["shadows"];
 
 declare module "@emotion/react" {
   export interface Theme {
     colors: { [K in ColorKeys]: (typeof theme)["colors"][K] };
-    shadows: {
-      1: string;
-      2: string;
-    };
+    shadows: { [K in ShadowKeys]: (typeof theme)["shadows"][K] };
   }
 }
