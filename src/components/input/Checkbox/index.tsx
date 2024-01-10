e530e3450bebb7baafe179e87a34272f9dc84308
Gaoridang <oingc@naver.com>
2024-01-10 14:18:18 +0900

    Feat: 하나의 체크박스 컴포넌트로 합치기

--- src/components/input/Checkbox/index.tsx
+++ src/components/input/Checkbox/index.tsx
@@ -1,5 +1,21 @@
-const Checkbox = () => {
-  return <div>Checkbox</div>;
+import AllConsentCheckbox from "./AllConsentCheckbox";
+import IndividualConsentCheckbox, {
+  type IndividualConsentCheckboxProps
+} from "./IndividualConsentCheckbox";
+
+type CheckboxProps =
+  | { variant: "all" }
+  | (IndividualConsentCheckboxProps & { variant: "individual" });
+
+const Checkbox = ({ variant = "all", ...individualProps }: CheckboxProps) => {
+  switch (variant) {
+    case "all":
+      return <AllConsentCheckbox />;
+    case "individual":
+      return <IndividualConsentCheckbox {...(individualProps as IndividualConsentCheckboxProps)} />;
+    default:
+      return <AllConsentCheckbox />;
+  }
 };
 
 export default Checkbox;
