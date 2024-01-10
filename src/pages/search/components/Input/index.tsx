be6312ccd0dae15aa5b8396c2c47792b71115cc4
gaori <monooi@naver.com>
2024-01-10 17:22:19 +0900

    Feat: 검색 페이지 검색 탭 제작

--- src/pages/search/components/Input/index.tsx
+++ src/pages/search/components/Input/index.tsx
@@ -1,15 +1,62 @@
+import { ReactNode } from "react";
 import * as S from "./styles";
-import SearchIcon from "@assets/icons/search_Search.svg?react";
+import ArrowDown from "@assets/icons/search_arrowDown.svg?react";
+import { useTheme } from "@emotion/react";
+import { AnimatePresence, motion } from "framer-motion";
 
-// Focus 애니메이션
-// border radius 208.017px..?
-const Input = () => {
+interface SearchInputProps {
+  leftPlaceholder: string;
+  rightPlaceholder: string;
+  children: ReactNode;
+  icon: ReactNode;
+  isOpen: boolean;
+  setIsOpen: () => void;
+}
+
+const SearchTab = ({
+  icon,
+  leftPlaceholder,
+  rightPlaceholder,
+  children,
+  isOpen,
+  setIsOpen
+}: SearchInputProps) => {
+  const theme = useTheme();
+
+  const handleClick = () => {
+    setIsOpen();
+  };
+
   return (
-    <S.Wrapper>
-      <SearchIcon />
-      <S.Input placeholder="원하는 장소 / 날짜 / 인원을 검색해보세요" />
-    </S.Wrapper>
+    <S.Container>
+      <S.Wrapper onClick={handleClick}>
+        <S.Flex>
+          <div>{icon}</div>
+          <S.Subtitle color={theme.colors.gray[700]}>{leftPlaceholder}</S.Subtitle>
+        </S.Flex>
+        <S.Flex>
+          <S.Subtitle color={theme.colors.gray[600]}>{rightPlaceholder}</S.Subtitle>
+          <div>
+            <ArrowDown />
+          </div>
+        </S.Flex>
+      </S.Wrapper>
+
+      <AnimatePresence>
+        {isOpen && (
+          <motion.div
+            initial={{ height: 0 }}
+            animate={{ height: "auto" }}
+            exit={{ height: 0, opacity: 0 }}
+            transition={{ duration: 0.2, ease: [0, 0.71, 0.2, 1.01] }}
+            style={{ overflow: "hidden" }}
+          >
+            {children}
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </S.Container>
   );
 };
 
-export default Input;
+export default SearchTab;
