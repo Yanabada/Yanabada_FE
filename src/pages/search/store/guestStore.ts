25132e69bb2cef661c7ccd2db0e8ca8b54db4f93
gaori <monooi@naver.com>
2024-01-11 20:06:38 +0900

    Feat: 게스트 숫자 상태 관리 스토어

---
+++ src/pages/search/store/guestStore.ts
@@ -0,0 +1,25 @@
+import { create } from "zustand";
+
+interface GuestState {
+  adult: number;
+  child: number;
+  increaseAdult: () => void;
+  decreaseAdult: () => void;
+  increaseChild: () => void;
+  decreaseChild: () => void;
+}
+
+export const useGuest = create<GuestState>()((set) => ({
+  adult: 2,
+  child: 0,
+  increaseAdult: () =>
+    set((state) => ({
+      adult: state.adult + 1
+    })),
+  decreaseAdult: () =>
+    set((state) => ({
+      adult: state.adult > 0 ? state.adult - 1 : 0
+    })),
+  increaseChild: () => set((state) => ({ child: state.child + 1 })),
+  decreaseChild: () => set((state) => ({ child: state.child > 0 ? state.child - 1 : 0 }))
+}));
