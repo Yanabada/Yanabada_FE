6f19f117af21ad0d6a39ea528ec7e17b0c1e5bd5
furaha707 <zxcvyein950823@gmail.com>
2024-01-12 01:27:11 +0900

    Feat: 공통 판매가격 인풋 및 모달제작달

--- src/components/notice/index.tsx
+++ src/components/notice/index.tsx
@@ -1,11 +1,19 @@
 import * as S from "./styles";
 import YanoljaIcon from "@assets/icons/YanoljaIcon.svg?react";
 import CautionIcon from "@assets/icons/CautionIcon.svg?react";
+import QuestionIcon from "@assets/icons/QuestionIcon.svg?react";
 
 interface YanoljaProps {
   type: "yanolja";
   content: string;
   shape?: "lineFill";
+}
+
+interface QuestionProps {
+  type: "question";
+  title: string;
+  content: string;
+  shape?: "line";
 }
 
 interface DefaultProps {
@@ -15,7 +23,7 @@ interface DefaultProps {
   shape?: "line" | "fill" | "lineFill";
 }
 
-type NoticeProps = YanoljaProps | DefaultProps;
+type NoticeProps = YanoljaProps | DefaultProps | QuestionProps;
 
 const Notice = (props: NoticeProps) => {
   switch (props.type) {
@@ -24,6 +32,16 @@ const Notice = (props: NoticeProps) => {
         <S.NoticeWrapper className={props.shape ? `${props.shape} yanolja` : "yanolja"}>
           <YanoljaIcon />
           <S.TextWrapper>{props.content}</S.TextWrapper>
+        </S.NoticeWrapper>
+      );
+    case "question":
+      return (
+        <S.NoticeWrapper className={props.shape ? `${props.shape} question` : "question"}>
+          <QuestionIcon />
+          <S.TextWrapper>
+            <S.NoticeTitle className={props.type}>{props.title}</S.NoticeTitle>
+            <S.TextWrapper>{props.content}</S.TextWrapper>
+          </S.TextWrapper>
         </S.NoticeWrapper>
       );
     default:
