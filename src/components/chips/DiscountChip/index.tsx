beb75b5beda729eccddf0cadf394e5beb530c74d
seungsimdang <oak20005@naver.com>
2024-01-11 16:54:21 +0900

    Comment: 주석 추가

--- src/components/chips/DiscountChip/index.tsx
+++ src/components/chips/DiscountChip/index.tsx
@@ -4,9 +4,11 @@ const DiscountChip = () => {
   return (
     <S.DiscountChipWrapper>
       <S.DiscountChipLeftSection>
+        {/* FIXME: 추후 변수로 받아서 대체 예정 */}
         <S.LeftSectionText>선착순</S.LeftSectionText>
       </S.DiscountChipLeftSection>
       <S.DiscountChipRightSection>
+        {/* FIXME: 추후 변수로 받아서 대체 예정 */}
         <S.RightSectionText>10%</S.RightSectionText>
       </S.DiscountChipRightSection>
     </S.DiscountChipWrapper>
