28ec5989f2b650484a1b11539c09ff805dfe1f80
SOL-MI <ovo10203@gmail.com>
2024-01-13 04:57:38 +0900

    Design: 회원가입 바텀시트 컴포넌트 분리

---
+++ src/pages/login/components/signInBottomSheet/signInBottomSheet.styles.ts
@@ -0,0 +1,9 @@
+import styled from "@emotion/styled";
+
+export const SheetBtnWrapper = styled.div`
+  padding: 65px 0 0;
+`;
+
+export const AgreementContainer = styled.div`
+  padding: 0.875rem 0.875rem 1.38rem;
+`;
