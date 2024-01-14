10e34012fab2a9d29d0ae79aa2320a44d8bff79b
seungsimdang <oak20005@naver.com>
2024-01-15 08:09:41 +0900

    Refactor: Info 컴포넌트 props로 내용 받을수 있도록 변경

--- src/pages/myPage/components/Info/index.tsx
+++ src/pages/myPage/components/Info/index.tsx
@@ -1,14 +1,51 @@
 import * as S from "./styles";
+import YanoljaIcon from "@assets/icons/YanoljaIcon.svg?react";
 
 interface PayInfoProps {
-  divType: "payInfo" | "transactionInfo";
+  divType: "payInfo" | "transactionInfo" | "payInfo-tall";
   width?: string;
   imageURL?: string;
+  yanoljaPurchasePrice?: string;
+  yanabadaPurchasePrice?: string;
+  discountRate?: number;
+  discountPrice?: string;
+  from?: string;
+  payMethod?: string;
+  accommodationName?: string;
+  roomName?: string;
+  orderNumber?: string;
+  buyer?: string;
+  orderDate?: string;
+  charge?: string;
 }
 
-const Info = ({ divType, width, imageURL }: PayInfoProps) => {
+const Info = ({
+  divType,
+  width,
+  imageURL,
+  yanoljaPurchasePrice,
+  yanabadaPurchasePrice,
+  discountRate,
+  discountPrice,
+  from,
+  payMethod,
+  accommodationName,
+  roomName,
+  orderNumber,
+  buyer,
+  orderDate,
+  charge
+}: PayInfoProps) => {
+  const textContent =
+    from === "sale"
+      ? "야나바다 판매가"
+      : from === "purchase" || from === "cancel"
+        ? "야나바다 구매가"
+        : "";
+
   switch (divType) {
     case "payInfo":
+    case "payInfo-tall":
       return (
         <S.InfoWrapper divType={divType} width={width}>
           <S.TopWrapper>
@@ -16,52 +53,69 @@ const Info = ({ divType, width, imageURL }: PayInfoProps) => {
           </S.TopWrapper>
           <S.SeperationForm>
             <S.FormLeftText color="gray">야놀자 구매가</S.FormLeftText>
-            <S.FormRightPrice color="black">1,200,000원</S.FormRightPrice>
-          </S.SeperationForm>
-          <S.SeperationForm isBorder={true}>
-            <S.FormTextWrapper>
-              <S.FormLeftText color="gray">양도할인가</S.FormLeftText>
-              <S.FormLeftText color="blue">(50% 할인)</S.FormLeftText>
-            </S.FormTextWrapper>
-            <S.FormRightPrice color="gray">-600,000원</S.FormRightPrice>
+            <S.FormRightPrice color="black">{yanoljaPurchasePrice}원</S.FormRightPrice>
           </S.SeperationForm>
           <S.SeperationForm>
-            <S.FormLeftTextBold>야나바다 판매가</S.FormLeftTextBold>
-            <S.FormRightPrice color="blue">600,000원</S.FormRightPrice>
+            <S.FormTextWrapper>
+              <S.FormLeftText color="gray">양도할인가</S.FormLeftText>
+              <S.FormLeftText color="blue">({discountRate}% 할인)</S.FormLeftText>
+            </S.FormTextWrapper>
+            <S.FormRightPrice color="gray">-{discountPrice}원</S.FormRightPrice>
+          </S.SeperationForm>
+
+          {from === "purchase" || from === "cancel" ? (
+            <S.SeperationForm>
+              <S.FormTextWrapper>
+                <S.FormLeftText color="gray">수수료</S.FormLeftText>
+                <S.FormLeftText color="red">(야놀자페이 무료)</S.FormLeftText>
+              </S.FormTextWrapper>
+              <S.FormRightPrice color="gray">{charge}원</S.FormRightPrice>
+            </S.SeperationForm>
+          ) : null}
+
+          <S.SeperationForm isBorder={true}>
+            <S.FormLeftTextBold>{textContent}</S.FormLeftTextBold>
+            <S.FormRightPrice color="blue">{yanabadaPurchasePrice}원</S.FormRightPrice>
           </S.SeperationForm>
           <S.SeperationForm>
             <S.FormLeftText>결제수단</S.FormLeftText>
-            <S.FormRightText>야놀자페이</S.FormRightText>
+            <S.FormRightText>{payMethod}</S.FormRightText>
           </S.SeperationForm>
         </S.InfoWrapper>
       );
     case "transactionInfo":
       return (
         <S.InfoWrapper divType={divType} width={width}>
-          <S.TopWrapper>
+          <S.TopWrapper width={width}>
             <S.TopLabel>거래 정보</S.TopLabel>
+            {from === "purchase" || from === "cancel" ? (
+              <S.ShortcutWrapper>
+                <S.ShortcutText>야놀자 바로가기</S.ShortcutText>
+                <YanoljaIcon />
+              </S.ShortcutWrapper>
+            ) : null}
           </S.TopWrapper>
           <S.MiddleWrapper isBorder={true}>
             <S.FormLeftText color="gray">상품명</S.FormLeftText>
             <S.MiddleBottomWrapper>
               <S.ImageWrapper imageURL={imageURL} />
               <S.MiddleBottomRightWrapper>
-                <S.AccomodationName>파라스파라 서울 더 그레이트 현대 런던 호텔</S.AccomodationName>
-                <S.RoomName>Forest Tower Deluxe King</S.RoomName>
+                <S.AccomodationName>{accommodationName}</S.AccomodationName>
+                <S.RoomName>{roomName}</S.RoomName>
               </S.MiddleBottomRightWrapper>
             </S.MiddleBottomWrapper>
           </S.MiddleWrapper>
           <S.SeperationForm>
             <S.FormLeftText>주문번호</S.FormLeftText>
-            <S.FormRightText>00000000</S.FormRightText>
+            <S.FormRightText>{orderNumber}</S.FormRightText>
           </S.SeperationForm>
           <S.SeperationForm>
             <S.FormLeftText>구매자</S.FormLeftText>
-            <S.FormRightText>USER12345</S.FormRightText>
+            <S.FormRightText>{buyer}</S.FormRightText>
           </S.SeperationForm>
           <S.SeperationForm>
             <S.FormLeftText>주문일시</S.FormLeftText>
-            <S.FormRightText>2024.01.06(토) 00:00</S.FormRightText>
+            <S.FormRightText>{orderDate}</S.FormRightText>
           </S.SeperationForm>
         </S.InfoWrapper>
       );
