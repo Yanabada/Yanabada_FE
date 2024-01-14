d2848597eed82c53411423acbd9a96c29b3b0c9d
wkdtnqls0506 <wkdtnqls0506@naver.com>
2024-01-15 02:47:17 +0900

    Feat: 전체 상품 리스트 타입 선언

---
+++ src/pages/products/types/productsType.ts
@@ -0,0 +1,21 @@
+export interface ProductItemsType {
+  id: number;
+  image: string;
+  accommodationName: string;
+  roomName: string;
+  address: string;
+  checkIn: string;
+  checkOut: string;
+  min: number;
+  max: number;
+  saleEnd: string;
+  rating: number;
+  salesPercentage: number;
+  canNegotiate: boolean;
+  price: number;
+  sellingPrice: number;
+  purchasePrice: number;
+  latitude: number;
+  longitude: number;
+  status: "ON_SALE" | "SOLD_OUT" | "BOOKING" | "CANCELED";
+}
