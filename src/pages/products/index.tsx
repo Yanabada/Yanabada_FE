eb58b56c8d485ad0523c304ec290acccee9eea9f
wkdtnqls0506 <wkdtnqls0506@naver.com>
2024-01-15 02:43:57 +0900

    Feat: /products api 생성 및 연동

--- src/pages/products/index.tsx
+++ src/pages/products/index.tsx
@@ -7,11 +7,26 @@ import * as S from "./styles/style";
 import KakaoMap from "./components/KakaoMap";
 import CategoryTab from "./components/CategoryTab";
 import GoToMapButton from "./components/ToMapButton";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import getProducts from "./api/products";
+import { ProductItemsType } from "@pages/products/types/productsType";
 
 const Products = () => {
   const [isMapOpen, setMapOpen] = useState(false);
+  const [productItem, setProductItem] = useState<ProductItemsType[] | null>(null);
+
+  useEffect(() => {
+    const productData = async () => {
+      try {
+        const data = await getProducts();
+        setProductItem(data);
+      } catch (error) {
+        console.error("/products error: ", error);
+      }
+    };
+    productData();
+  }, []);
 
   return (
     <>
@@ -32,10 +47,10 @@ const Products = () => {
           <S.SecondContainer>
             <OptionTab />
             <Order />
+            <S.ProductCardWrapper>
+              {productItem?.map((product: ProductItemsType) => <ProductCard product={product} />)}
+            </S.ProductCardWrapper>
           </S.SecondContainer>
-          <S.ProductCardWrapper>
-            <ProductCard />
-          </S.ProductCardWrapper>
         </>
       )}
       {!isMapOpen && <GoToMapButton handleClick={() => setMapOpen(true)} />}
