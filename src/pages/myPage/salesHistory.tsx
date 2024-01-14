8070a644547ef96c06c4efa32e4e463c5d247d83
seungsimdang <oak20005@naver.com>
2024-01-14 13:34:33 +0900

    Chore: 불필요 코드 삭제

--- src/pages/myPage/salesHistory.tsx
+++ src/pages/myPage/salesHistory.tsx
@@ -1,5 +1,4 @@
 import * as S from "./styles/salesHistory.styles";
-import { Link } from "react-router-dom";
 import UpperNavBar from "@components/navBar/upperNavBar";
 import ListCard from "@components/card/ListCard";
 
