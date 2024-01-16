c4979ff643a69003a136c16d0e091a23268dffbd
seungsimdang <oak20005@naver.com>
2024-01-16 19:54:00 +0900

    Feat: FCM init

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { RouterProvider } from "react-router-dom";
@@ -7,6 +6,7 @@ import theme from "themes/theme";
 import "./main.css";
 import { Global, ThemeProvider } from "@emotion/react";
 import globalStyles from "styles/globalStyles";
+import { requestPermission } from "./firebase-messaging-sw.js";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -16,13 +16,13 @@ const queryClient = new QueryClient({
   }
 });
 
+requestPermission();
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <ThemeProvider theme={theme}>
-        <Global styles={globalStyles} />
-        <RouterProvider router={router} />
-      </ThemeProvider>
-    </QueryClientProvider>
-  </React.StrictMode>
+  <QueryClientProvider client={queryClient}>
+    <ThemeProvider theme={theme}>
+      <Global styles={globalStyles} />
+      <RouterProvider router={router} />
+    </ThemeProvider>
+  </QueryClientProvider>
 );
