0d57c75195e4d0ab7d218ba3bfea1e8ccdc70cdf
wkdtnqls0506 <wkdtnqls0506@naver.com>
2024-01-17 00:47:54 +0900

    Feat: Say 공통 컴포넌트화

--- src/pages/productDetail/components/SellerSay/index.tsx
+++ src/pages/productDetail/components/SellerSay/index.tsx
@@ -1,4 +1,5 @@
 import * as S from "./style";
+import Say from "@components/say";
 import { SellerType } from "@pages/productDetail/types/detailType";
 
 interface SellerProps {
@@ -13,12 +14,7 @@ const SellerSay = ({ seller, description }: SellerProps) => {
         <S.Text>판매자 한마디</S.Text>
         <S.NickName>{seller.nickname} 님</S.NickName>
       </S.NickNameContainer>
-      <S.SayContainer>
-        <S.UserImage src={seller.imageUrl} />
-        <S.SayInnerContainer>
-          <S.SellerText>{description}</S.SellerText>
-        </S.SayInnerContainer>
-      </S.SayContainer>
+      <Say seller={seller} description={description} />
     </S.Container>
   );
 };
--- src/pages/productDetail/components/SellerSay/style.ts
+++ src/pages/productDetail/components/SellerSay/style.ts
@@ -22,33 +22,3 @@ export const NickName = styled.p`
   color: ${({ theme }) => theme.colors.gray[700]};
   ${({ theme }) => theme.text.body2}
 `;
-
-export const SayContainer = styled.div`
-  display: flex;
-  gap: 0.5rem;
-`;
-
-export const UserImage = styled.img`
-  width: 2.1875rem;
-  height: 2.1875rem;
-  border-radius: 50%;
-`;
-
-export const SayInnerContainer = styled.div`
-  display: flex;
-  padding: 1rem;
-  justify-content: flex-start;
-  align-items: center;
-  gap: 4px;
-  flex: 1 0 0;
-  border-radius: 0px 20px 20px 20px;
-  background: #fff;
-
-  /* 은은한쉐도우 */
-  box-shadow: ${({ theme }) => theme.shadows[3]};
-`;
-
-export const SellerText = styled.p`
-  color: ${({ theme }) => theme.colors.gray[700]};
-  ${({ theme }) => theme.text.body2}
-`;
