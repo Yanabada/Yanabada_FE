5e25b4f7f27736aa29a64e18dc9b8bb3e8f36f7c
wkdtnqls0506 <wkdtnqls0506@naver.com>
2024-01-16 14:11:43 +0900

    Feat: ProductDetail 페이지 생성

---
+++ src/pages/productDetail/index.tsx
@@ -0,0 +1,40 @@
+import * as S from "./styles/style";
+import UpperNavBar from "@components/navBar/upperNavBar";
+import DetailInfo from "./components/DetailInfo";
+import SellerSay from "./components/SellerSay";
+import Location from "./components/Location";
+import Information from "./components/Information";
+import ServiceInfo from "./components/ServiceInfo";
+import Notice from "./components/Notice";
+import Footer from "./components/Footer";
+import NegoButton from "./components/NegoButton";
+import useProductDetail from "./hooks/useProductDetail";
+
+const ProductDetail = () => {
+  const {
+    getDetailQuery: { data: detailData }
+  } = useProductDetail();
+
+  return (
+    <S.Container>
+      <UpperNavBar title="상품 상세" type="back" />
+      {detailData && (
+        <>
+          <DetailInfo data={detailData} />
+          <SellerSay seller={detailData.seller} description={detailData.description} />
+          <Location accommodationInfo={detailData.accommodationInfo} />
+          <Information accommodationInfo={detailData.accommodationInfo} />
+          <ServiceInfo
+            accommodationInfo={detailData.accommodationInfo}
+            roomInfo={detailData.roomInfo}
+          />
+          <Notice roomInfo={detailData.roomInfo} />
+          <Footer phoneNumber={detailData.accommodationInfo.phoneNumber} />
+          <NegoButton data={detailData} />
+        </>
+      )}
+    </S.Container>
+  );
+};
+
+export default ProductDetail;
