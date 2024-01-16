f8e069e5c1340b4e8335868423ff6d1ee2b76aa6
SOL-MI <ovo10203@gmail.com>
2024-01-16 13:30:00 +0900

    Design: 채팅방리스트 디자인 완료

---
+++ src/pages/chat/components/chatList/index.tsx
@@ -0,0 +1,34 @@
+import { useNavigate } from "react-router-dom";
+import * as S from "./styles";
+import NumberBadge from "@components/numberBadge";
+
+interface ChatListProps {
+  roomId: number;
+}
+const ChatList = ({ roomId }: ChatListProps) => {
+  const navigate = useNavigate();
+
+  return (
+    <S.List onClick={() => navigate(`/${roomId}`)}>
+      <S.ProfileImg
+        src="https://i.pinimg.com/564x/4d/a5/dd/4da5dd3d7b0a981cd56ec1f7db32c0cf.jpg"
+        alt="프로필"
+      />
+      <S.DetailContainer>
+        <S.LeftContainer>
+          <S.NameContainer>
+            <S.NickName>강쥐사랑해진짜로</S.NickName>
+            <S.RoomName>파라스파라 서울 더 그레이트 현대 런던 </S.RoomName>
+          </S.NameContainer>
+          <S.ChatText>만원만 깎아줘</S.ChatText>
+        </S.LeftContainer>
+        <S.RightContainer>
+          <S.TimeText>오후5:50</S.TimeText>
+          <NumberBadge number={3} />
+        </S.RightContainer>
+      </S.DetailContainer>
+    </S.List>
+  );
+};
+
+export default ChatList;
--- src/pages/chat/index.tsx
+++ src/pages/chat/index.tsx
@@ -1,13 +1,17 @@
 import UpperNavBar from "@components/navBar/upperNavBar";
-import NoChatList from "./components/noChatList";
+// import NoChatList from "./components/noChatList";
 import * as S from "./styles/styles";
+import ChatList from "./components/chatList";
 
 const Chat = () => {
+  // TODO - 데이터 없으면 NoChatList로 return
   return (
     <>
-      <UpperNavBar title="채팅" type="back" />
+      <UpperNavBar title="채팅" type="back" hasBorder />
       <S.ChatContainer>
-        <NoChatList type="noLogin" />
+        <ChatList roomId={1} />
+        <ChatList roomId={2} />
+        {/* <NoChatList type="noLogin" /> */}
       </S.ChatContainer>
     </>
   );
