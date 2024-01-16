fa4e4b32a6bcbbee785c5a5b4f66a699b5ef68e1
furaha707 <zxcvyein950823@gmail.com>
2024-01-16 16:46:45 +0900

    Design: 확인 및 완료 페이지 2p 구현

--- src/components/buttons/DateChangeButton/index.tsx
+++ src/components/buttons/DateChangeButton/index.tsx
@@ -4,18 +4,19 @@ import { FaRegCalendarAlt } from "react-icons/fa";
 interface DateChangeButtonProps {
   endDate: string;
   width?: string;
+  disabled?: boolean | undefined;
   onClick?: () => void;
 }
 
-const DateChangeButton = ({ width, endDate, onClick }: DateChangeButtonProps) => {
+const DateChangeButton = ({ width, endDate, disabled, onClick }: DateChangeButtonProps) => {
   return (
     <S.DateChangeButton width={width} onClick={onClick}>
       <S.LeftSection>
         <FaRegCalendarAlt size="18px" />
-        <S.Label>{endDate} (월)</S.Label>
-        <S.DescriptionLabel>00시00분 종료</S.DescriptionLabel>
+        <S.Label disabled={disabled}>{endDate} (월)</S.Label>
+        <S.DescriptionLabel disabled={disabled}>00시00분 종료</S.DescriptionLabel>
       </S.LeftSection>
-      <S.RightButton>변경</S.RightButton>
+      {!disabled && <S.RightButton>변경</S.RightButton>}
     </S.DateChangeButton>
   );
 };
