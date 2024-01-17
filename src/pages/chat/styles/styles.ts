71ffe6d6497b1e0cd0e78bdb6c599dd71fe9b54b
고솔미 (Solmi Koh) <113079102+SOL-MI@users.noreply.github.com>
2024-01-17 14:03:02 +0900

    Design : 채팅방 배너 및 모달 디자인

--- src/pages/chat/styles/styles.ts
+++ src/pages/chat/styles/styles.ts
@@ -3,7 +3,4 @@ import styled from "@emotion/styled";
 export const ChatContainer = styled.div`
   height: 100%;
   background-color: ${({ theme }) => theme.colors.gray[200]};
-  > div:first-of-type {
-    border-top: 2px solid ${({ theme }) => theme.colors.gray[100]};
-  }
 `;
