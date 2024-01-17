01c79e08dc653e2e0710f8eac3252f229b8b209c
wkdtnqls0506 <wkdtnqls0506@naver.com>
2024-01-18 01:15:33 +0900

    Design: Home 페이지 생성

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,12 +1,20 @@
+import * as S from "./styles/style";
 import Header from "./components/Header";
 import SearchInput from "./components/SearchInput";
+import Banner from "./components/Banner";
+import RecentlyProduct from "./components/RecentlyProduct";
+import CategoryProduct from "./components/CategoryProduct";
 
 const Home = () => {
   return (
-    <>
+    <S.Container>
       <Header />
       <SearchInput />
-    </>
+      <Banner />
+      <RecentlyProduct />
+      <CategoryProduct title="마감 임박 상품" link="/products" rowCardProps="마감임박" />
+      <CategoryProduct title="높은 할인율 상품" link="/products" rowCardProps="높안할인율" />
+    </S.Container>
   );
 };
 
