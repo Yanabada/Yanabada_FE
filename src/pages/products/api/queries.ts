7b83378407ac827ffab0559d8b61a513c6b5eb21
gaori <monooi@naver.com>
2024-01-18 12:40:09 +0900

    Refactor: 파라미터 받을 수 있게 useQuery 함수 변경
    무한 스크롤을 위한 useInfiniteQuery 설정

--- src/pages/products/api/queries.ts
+++ src/pages/products/api/queries.ts
@@ -1,16 +1,24 @@
-import { useSuspenseInfiniteQuery } from "@tanstack/react-query";
-import getProducts from "./products";
-import { ProductType } from "../types/productsType";
+import { InfiniteData, useSuspenseInfiniteQuery } from "@tanstack/react-query";
+import getProducts, { GetProductsRequestParams } from "./products";
+import { GetProductResponseData } from "../types/productsType";
 import { AxiosError } from "axios";
 
 // FIXME: Response 데이터에 totalPage, totalElements 추가 필요
-const useProducts = () => {
-  return useSuspenseInfiniteQuery<ProductType[], AxiosError, ProductType[], Array<string>, number>({
+const useProducts = (params: Partial<GetProductsRequestParams>) => {
+  return useSuspenseInfiniteQuery<
+    GetProductResponseData,
+    AxiosError,
+    InfiniteData<GetProductResponseData>,
+    Array<string>,
+    number
+  >({
     queryKey: ["products"],
-    queryFn: ({ pageParam }) => getProducts({ page: pageParam }),
+    queryFn: ({ pageParam }) => getProducts({ ...params, page: pageParam }),
     initialPageParam: 1,
-    getNextPageParam: () => {
-      return undefined;
+    getNextPageParam: (lastPage, allPages) => {
+      const isLastPage = allPages.length === lastPage.totalPages;
+      if (isLastPage) return undefined;
+      return allPages.length + 1;
     }
   });
 };
