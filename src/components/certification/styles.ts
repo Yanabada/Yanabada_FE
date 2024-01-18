7bfa9b454b6bd6d0024ae3dc5bc77306449ad11c
222SeungHyun <93538221+seungsimdang@users.noreply.github.com>
2024-01-18 19:22:24 +0900

    Design: 예약자 정보 수정 페이지 디자인

---
+++ src/components/certification/styles.ts
@@ -0,0 +1,54 @@
+import styled from "@emotion/styled";
+import { css } from "@emotion/react";
+
+interface CertificationProps {
+  width?: string;
+  gap?: string;
+}
+
+const TextLayout = css`
+  font-size: 12px;
+  font-style: normal;
+  line-height: 17px; /* 141.667% */
+`;
+
+export const CertificationContainer = styled.div`
+  display: flex;
+  height: 100%;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+export const CertificationWrapper = styled.div<CertificationProps>`
+  width: ${({ width }) => width || "360px"};
+  display: flex;
+  padding: 20px 14px;
+  flex-direction: column;
+  gap: ${({ gap }) => gap || "0px"};
+  background-color: #fff;
+`;
+
+export const CertificationText = styled.span`
+  ${TextLayout}
+
+  color: ${({ theme }) => theme.colors.gray[700]};
+  font-weight: 400;
+`;
+
+export const CertificationTextBold = styled.span`
+  ${TextLayout}
+
+  color: ${({ theme }) => theme.colors.gray[700]};
+  font-weight: 700;
+`;
+
+export const ButtonWrapper = styled.div`
+  display: inline-flex;
+  padding: 16px 14px;
+  flex-direction: column;
+  align-items: center;
+  gap: 16px;
+  bottom: 0;
+
+  border-top: 1px solid ${({ theme }) => theme.colors.gray[300]};
+`;
