b522310089241e739bb5f4cfa704a4a82a66542a
고솔미 (Solmi Koh) <113079102+SOL-MI@users.noreply.github.com>
2024-01-20 04:15:34 +0900

    Feat: 회원가입 닉네임 유효성 검사

--- src/pages/signIn/components/signInThird/SignInThird.tsx
+++ src/pages/signIn/components/signInThird/SignInThird.tsx
@@ -2,42 +2,86 @@ import TextInput from "@components/input/TextInput";
 import Notice from "@components/notice";
 import * as S from "./signInThird.styles";
 import AuthenticationButton from "@components/buttons/AuthenticationButton";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ColoredButtonForm from "@components/buttons/ColoredButtonForm";
 import UpperNavBar from "@components/navBar/upperNavBar";
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
 
 const SignInThird = () => {
-  // TODO - 유효성 검사
-  const [isValid] = useState(false);
-  const [isMultiple] = useState(false);
+  const navigate = useNavigate();
+  const [isValid, setIsValid] = useState(false);
+  const [isMultiple, setIsMultiple] = useState(true);
 
-  const errMsg = (): string => {
-    // TODO - value없으면 early return
-    if (!isValid) {
-      return "닉네임 규칙을 확인해주세요.";
-    } else if (isMultiple) {
-      return "이미 사용중인 닉네임입니다.";
+  const {
+    register,
+    formState: { errors },
+    getValues,
+    setError
+  } = useForm({
+    mode: "onBlur"
+  });
+  const nickname = getValues("nickname");
+
+  useEffect(() => {
+    if (!errors.nickname && nickname) {
+      setIsValid(true);
     }
-    return "";
+  }, [nickname, errors.nickname]);
+
+  const handleCheckMultiple = () => {
+    // TODO - 이미 존재하는 닉네밍인지 체크하는 로직
+    // 실패시 early return;
+    setError("nickname", {
+      type: "manual",
+      message: "이미 사용중인 닉네임입니다."
+    });
+    // 성공시
+    setIsMultiple(false);
   };
 
   return (
     <>
       <UpperNavBar type="back" title={`회원가입(3/4)`} />
 
-      <TextInput variant="move" label="닉네임" errorMessage={errMsg()} />
+      <TextInput
+        variant="move"
+        label="닉네임"
+        {...register("nickname", {
+          required: true,
+          pattern: {
+            value: /^[A-Za-z가-힣]{2,8}$/,
+            message: "닉네임 규칙을 확인해주세요."
+          }
+        })}
+        errorMessage={errors.nickname && `${errors.nickname?.message}`}
+      />
       <S.NoticeContainer>
         <Notice
           type="default"
           color="orange"
-          title="닉네임은 한글과 영문만을 사용하여 3글자 이상, 10글자 이하여야 합니다."
+          title="닉네임은 한글과 영문만을 사용하여 2글자 이상, 8글자 이하여야 합니다."
         />
       </S.NoticeContainer>
-      <AuthenticationButton buttonType={isValid ? "disabled" : "default"} width="100%">
+      <AuthenticationButton
+        buttonType={isValid ? "disabled" : "default"}
+        width="100%"
+        onClick={handleCheckMultiple}
+      >
         중복 확인
       </AuthenticationButton>
 
-      <ColoredButtonForm isBottom={true} width="100%">
+      <ColoredButtonForm
+        isBottom={true}
+        width="100%"
+        isActive={!isMultiple}
+        onClick={() => {
+          if (isMultiple) {
+            return;
+          }
+          navigate("/sign-in/3");
+        }}
+      >
         다음
       </ColoredButtonForm>
     </>
