c60f8acccbe02381fb5da4afafd270b8b424ec5c
고솔미 (Solmi Koh) <113079102+SOL-MI@users.noreply.github.com>
2024-01-20 02:39:17 +0900

    Feat: 회원가입 이메일입력 페이지 유효성 검사 및 버튼&인풋 공통 컴포넌트 수정

--- src/pages/signIn/components/signInFirst/SignInFirst.tsx
+++ src/pages/signIn/components/signInFirst/SignInFirst.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import TextInput from "@components/input/TextInput";
 import Notice from "@components/notice";
 import * as S from "./signInFirst.styles";
@@ -6,17 +6,42 @@ import AuthenticationButton from "@components/buttons/AuthenticationButton";
 import ColoredButtonForm from "@components/buttons/ColoredButtonForm";
 import { Link } from "react-router-dom";
 import UpperNavBar from "@components/navBar/upperNavBar";
+import { useForm } from "react-hook-form";
 
 const SignInFirst = () => {
-  const [emailInput, setEmailInput] = useState("");
-  const [numInput, setNumInput] = useState("");
-  // TODO - 유효성 검사 통과 & emailInput 존재할 때 setIsSuccess(true);
-  const [isSuccess] = useState(true);
+  const [isSuccess, setIsSuccess] = useState(false);
   const [sentCount, setSentCount] = useState(5);
   const [btnText, setBtnText] = useState("인증번호 전송");
   const [openInput, setOpenInput] = useState(false);
-  // TODO - 유효성 검사 추가 & 판단 시점 여쭤보고 반영
-  const [isNumCorrect] = useState(false);
+  const [isNumCorrect, setIsNumCorrect] = useState(false);
+
+  const {
+    register,
+    formState: { errors },
+    getValues
+  } = useForm({
+    mode: "onBlur"
+  });
+  const email = getValues("email");
+
+  useEffect(() => {
+    if (!errors.email && email) {
+      setIsSuccess(true);
+      return;
+    }
+    setIsSuccess(false);
+  }, [email]);
+
+  // 인증번호 유효성검사
+  const isCodeValid = (value: number) => {
+    //  TODO - 인증번호 같은지 비교하는 로직 추가
+    if (value.toString().length === 6) {
+      setIsNumCorrect(true);
+      return true;
+    }
+    setIsNumCorrect(false);
+    return false;
+  };
 
   const handleAuthenticationBtnClick = () => {
     // TODO - 인증번호 재전송 5회 소진시 동작 기획에 맞게 수정
@@ -34,14 +59,18 @@ const SignInFirst = () => {
       <UpperNavBar type="back" title={`회원가입(1/4)`} />
 
       <TextInput
-        value={emailInput}
-        onChange={(e) => {
-          setEmailInput(e.target.value);
-        }}
         variant="move"
         label="이메일"
         isSuccess={isSuccess}
-        errorMessage={emailInput && !isSuccess ? "이메일이 올바르지 않습니다." : ""}
+        {...register("email", {
+          required: true,
+          pattern: {
+            // 8-20자 사이, 최소 하나의 숫자, 영문자, 특수문자를 포함, 공백 X
+            value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,}$/i,
+            message: "유효한 이메일 주소를 입력하세요"
+          }
+        })}
+        errorMessage={errors.email && `${errors.email?.message}`}
       />
       <Notice
         type="default"
@@ -62,13 +91,16 @@ const SignInFirst = () => {
         <S.InputContainer>
           {/* TODO - 시간제한 추가 */}
           <TextInput
-            value={numInput}
-            onChange={(e) => {
-              setNumInput(e.target.value);
-            }}
+            type="number"
             variant="move"
             label="인증번호 6자리"
-            errorMessage={numInput && !isNumCorrect ? "인증번호가 올바르지 않습니다." : ""}
+            {...register("code", {
+              required: true,
+              validate: {
+                validCode: (value) => isCodeValid(value) || "인증번호가 올바르지 않습니다."
+              }
+            })}
+            errorMessage={errors.code && `${errors.code?.message}`}
           />
         </S.InputContainer>
       )}
@@ -81,7 +113,7 @@ const SignInFirst = () => {
         못했다면 스팸함 또는 해당 이메일 서비스의 설정을 확인해주세요.
       </S.InformText>
       <Link to="/signin/1">
-        <ColoredButtonForm isBottom={true} width="100%">
+        <ColoredButtonForm isBottom={true} width="100%" isActive={isNumCorrect}>
           다음
         </ColoredButtonForm>
       </Link>
