d4dd04ba8dfaae4ed2e5798c43c7237c6fc3fe0e
고솔미 (Solmi Koh) <113079102+SOL-MI@users.noreply.github.com>
2024-01-20 03:19:37 +0900

    Feat: 회원가입 비밀번호 입력 유효성 검사

--- src/pages/signIn/components/signInSecond/SignInSecond.tsx
+++ src/pages/signIn/components/signInSecond/SignInSecond.tsx
@@ -1,34 +1,83 @@
 import TextInput from "@components/input/TextInput";
 import Notice from "@components/notice";
 import * as S from "./signInSecond.styles";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import ColoredButtonForm from "@components/buttons/ColoredButtonForm";
 import UpperNavBar from "@components/navBar/upperNavBar";
+import { useForm } from "react-hook-form";
+import { useState } from "react";
 
 const SignInSecond = () => {
+  const navigate = useNavigate();
+  const {
+    register,
+    formState: { errors },
+    getValues
+  } = useForm({
+    mode: "onBlur"
+  });
+
+  const [isPassed, setIsPassed] = useState(false);
+
+  const password = getValues("password");
+
+  const isCodeValid = (value: string) => {
+    console.log(value, "value");
+    if (value === password) {
+      setIsPassed(true);
+      return true;
+    }
+    return false;
+  };
+
   return (
     <>
       <UpperNavBar type="back" title={`회원가입(2/4)`} />
 
-      <TextInput variant="move" label="비밀번호" errorMessage={"비밀번호 규칙을 확인해주세요"} />
+      <TextInput
+        variant="move"
+        label="비밀번호"
+        {...register("password", {
+          required: true,
+          pattern: {
+            value: /^(?=.*[A-Za-z])(?=.*\d)(?=.*[!@#$%^&*()-_+=])[A-Za-z\d!@#$%^&*()-_+=]{8,20}$/,
+            message: "비밀번호가 올바르지 않습니다."
+          }
+        })}
+        errorMessage={errors.password && `${errors.password?.message}`}
+      />
       <S.NoticeContainer>
         <Notice
           type="default"
           color="orange"
-          title="비밀번호는 영문, 숫자, 특수문자 중 2종류 이상을 조합하여 최소 10자리 이상으로 입력해주세요."
+          title="비밀번호는 8-20자 사이여야 하며, 최소 하나의 숫자, 영문자, 특수문자를 포함하여야 합니다."
         />
       </S.NoticeContainer>
       <TextInput
         variant="move"
         label="비밀번호 확인"
-        errorMessage={"동일한 비밀번호를 입력해주세요"}
+        {...register("secondPassword", {
+          required: true,
+          validate: {
+            validCode: (value) => isCodeValid(value) || "동일한 비밀번호를 입력해주세요."
+          }
+        })}
+        errorMessage={errors.secondPassword && `${errors.secondPassword?.message}`}
       />
 
-      <Link to="/signin/2">
-        <ColoredButtonForm isBottom={true} width="100%">
-          다음
-        </ColoredButtonForm>
-      </Link>
+      <ColoredButtonForm
+        isBottom={true}
+        width="100%"
+        isActive={isPassed}
+        onClick={() => {
+          if (!isPassed) {
+            return;
+          }
+          navigate("/signin/2");
+        }}
+      >
+        다음
+      </ColoredButtonForm>
     </>
   );
 };
