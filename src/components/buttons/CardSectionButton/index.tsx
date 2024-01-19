b37365dd64996a28b4c0834e85c036e1ff721ca9
seungsimdang <oak20005@naver.com>
2024-01-20 02:06:28 +0900

    Feat: 마이페이지(G-1) 이동 로직 구현

--- src/components/buttons/CardSectionButton/index.tsx
+++ src/components/buttons/CardSectionButton/index.tsx
@@ -2,24 +2,29 @@ import * as S from "./styles";
 import { IoIosArrowForward } from "react-icons/io";
 import { IoIosSearch } from "react-icons/io";
 import YanoljaIcon from "@assets/icons/yanolja_Icon.svg?react";
+import { useNavigate } from "react-router-dom";
+
 interface CardSectionProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-  buttonType: "abledPay" | "disabledPay" | "abledPoint" | "disabledPoint";
+  buttonType: "abledPay" | "disabledPay" | "abledPoint" | "disabledPoint" | "management";
   width?: string;
 }
-const CardSectionButton = ({ buttonType, width }: CardSectionProps) => {
+
+const CardSectionButton = ({ buttonType, width, onClick }: CardSectionProps) => {
+  const navigate = useNavigate();
+
   switch (buttonType) {
     case "abledPay":
       return (
         <S.CardSectionWrapper buttonType={buttonType} width={width}>
           <S.ListButtonWrapper>
-            <S.ListButton>
+            <S.ListButton onClick={() => navigate("/charge/list")}>
               <S.ListButtonTextWrapper>
                 <YanoljaIcon />
                 <S.ListButtonText>야놀자 페이</S.ListButtonText>
               </S.ListButtonTextWrapper>
               <S.RightSectionWrapper>
                 {/* FIXME: 추후 변수값으로 대체 예정 */}
-                <S.RightSectionText>0원</S.RightSectionText>
+                <S.RightSectionText>350,000원</S.RightSectionText>
                 <IoIosArrowForward size="18px" color="#616161" />
               </S.RightSectionWrapper>
             </S.ListButton>
@@ -27,22 +32,21 @@ const CardSectionButton = ({ buttonType, width }: CardSectionProps) => {
           <S.BottomSectionWrapper>
             <S.BottomLeftButton buttonType={buttonType}>
               <IoIosSearch size="18px" color="#DE2E5F" />
-              <S.BottomLeftButtonText buttonType={buttonType}>출금하기</S.BottomLeftButtonText>
+              <S.BottomLeftButtonText buttonType={buttonType}>잔액인출</S.BottomLeftButtonText>
             </S.BottomLeftButton>
-            <S.BottomRightButton buttonType={buttonType}>
+            <S.BottomRightButton buttonType={buttonType} onClick={() => navigate("/charge")}>
               <IoIosSearch size="18px" color="#FFFFFF" />
-              <S.BottomRightButtonText>충전하기</S.BottomRightButtonText>
+              <S.BottomRightButtonText>충전</S.BottomRightButtonText>
             </S.BottomRightButton>
           </S.BottomSectionWrapper>
         </S.CardSectionWrapper>
       );
     case "disabledPay":
       return (
-        <S.CardSectionWrapper buttonType={buttonType} width={width}>
+        <S.CardSectionWrapper buttonType={buttonType} width={width} onClick={onClick}>
           <S.ListButtonWrapper>
             <S.ListButton>
               <S.ListButtonText>야놀자 페이</S.ListButtonText>
-
               <S.RightSectionWrapper>
                 <S.RightSectionText>로그인 후 이용가능</S.RightSectionText>
                 <IoIosArrowForward size="18px" color="#616161" />
@@ -51,13 +55,10 @@ const CardSectionButton = ({ buttonType, width }: CardSectionProps) => {
           </S.ListButtonWrapper>
           <S.BottomSectionWrapper>
             <S.BottomLeftButton buttonType={buttonType}>
-              <IoIosSearch size="18px" color="#9C9C9C" />
-              <S.BottomLeftButtonText buttonType={buttonType}>출금하기</S.BottomLeftButtonText>
+              <S.BottomLeftButtonText buttonType={buttonType}>
+                야놀자 페이 가입
+              </S.BottomLeftButtonText>
             </S.BottomLeftButton>
-            <S.BottomRightButton buttonType={buttonType}>
-              <IoIosSearch size="18px" color="#FFFFFF" />
-              <S.BottomRightButtonText>충전하기</S.BottomRightButtonText>
-            </S.BottomRightButton>
           </S.BottomSectionWrapper>
         </S.CardSectionWrapper>
       );
@@ -81,12 +82,33 @@ const CardSectionButton = ({ buttonType, width }: CardSectionProps) => {
       );
     case "disabledPoint":
       return (
+        <S.CardSectionWrapper buttonType={buttonType} width={width} onClick={onClick}>
+          <S.ListButtonWrapper>
+            <S.ListButton>
+              <S.ListButtonTextWrapper>
+                <YanoljaIcon />
+                <S.ListButtonText>야놀자 포인트</S.ListButtonText>
+              </S.ListButtonTextWrapper>
+              <S.RightSectionWrapper>
+                <S.RightSectionText>0 포인트</S.RightSectionText>
+                <IoIosArrowForward size="18px" color="#616161" />
+              </S.RightSectionWrapper>
+            </S.ListButton>
+          </S.ListButtonWrapper>
+        </S.CardSectionWrapper>
+      );
+    case "management":
+      return (
         <S.CardSectionWrapper buttonType={buttonType} width={width}>
           <S.ListButtonWrapper>
             <S.ListButton>
-              <S.ListButtonText>야놀자 포인트</S.ListButtonText>
+              <S.ListButtonTextWrapper>
+                {/* FIXME: 아이콘 변경 필요 */}
+                <YanoljaIcon />
+                <S.ListButtonText>승인요청관리</S.ListButtonText>
+              </S.ListButtonTextWrapper>
               <S.RightSectionWrapper>
-                <S.RightSectionText>로그인 후 이용가능</S.RightSectionText>
+                {/* FIXME: 추후 변수값으로 대체 예정 */}
                 <IoIosArrowForward size="18px" color="#616161" />
               </S.RightSectionWrapper>
             </S.ListButton>
--- src/pages/myPage/index.tsx
+++ src/pages/myPage/index.tsx
@@ -4,9 +4,14 @@ import ArrowForwardIcon from "@assets/icons/arrowForwardIcon.svg?react";
 import CardSectionButton from "@components/buttons/CardSectionButton";
 import ListButton from "@components/buttons/ListButton";
 import { useState } from "react";
+import { Link } from "react-router-dom";
 // import { registerServiceWorker } from "@utils/notification";
 // import { AppCheckTokenResult } from "@firebase/app-check-types";
 // import { getDeviceToken } from "../../firebase";
+import Modal from "@components/modal";
+import { useNavigate } from "react-router-dom";
+import NumberBadge from "@components/numberBadge";
+import { FaRegBell } from "react-icons/fa";
 
 interface MyPageProps {
   width?: string;
@@ -14,33 +19,96 @@ interface MyPageProps {
 
 const MyPage = ({ width }: MyPageProps) => {
   // FIXME: 추후 로그인 여부에 따라 상태 변경 예정
-  const [isLoginned] = useState(true);
+  const [isLoginned, setIsLoginned] = useState(true);
+  const [isLoginModalVisible, setIsLoginModalVisible] = useState(false);
+  const [isLogoutModalVisible, setIsLogoutModalVisible] = useState(false);
+
+  const navigate = useNavigate();
+
+  const loginModalProps = {
+    title: "회원가입 또는 로그인 후 사용할 수 있습니다.",
+    leftBtnText: "취소",
+    rightBtnText: "로그인/가입",
+    isVisible: isLoginModalVisible,
+    setIsVisible: setIsLoginModalVisible,
+    leftAction: () => setIsLoginModalVisible(false),
+    rightAction: () => navigate("/login")
+  };
+
+  const logoutModalProps = {
+    title: "로그아웃 하시겠습니까?",
+    leftBtnText: "취소",
+    rightBtnText: "확인",
+    isVisible: isLogoutModalVisible,
+    setIsVisible: setIsLogoutModalVisible,
+    leftAction: () => setIsLogoutModalVisible(false),
+    rightAction: () => {
+      setIsLoginned(false);
+      navigate("/mypage");
+    }
+  };
 
   return isLoginned ? (
     <>
-      <UpperNavBar title="MY 야나바다" type="back" />
+      <UpperNavBar
+        title="MY 야나바다"
+        type="back"
+        rightElement={
+          <>
+            <S.BellContainer>
+              <NumberBadge number={6} />
+              <FaRegBell className="bell" />
+            </S.BellContainer>
+          </>
+        }
+      />
       <S.MyPageContainer>
         <S.LoginButtonWrapper>
-          <S.LoginButton>
-            {/* FIXME: 추후 변수값으로 대체 예정 */}
-            USER123456 <ArrowForwardIcon />
-          </S.LoginButton>
+          <Link to="/mypage/profile">
+            <S.LoginButton>
+              {/* FIXME: 추후 변수값으로 대체 예정 */}
+              USER123456 <ArrowForwardIcon />
+            </S.LoginButton>
+          </Link>
         </S.LoginButtonWrapper>
 
         <CardSectionButton buttonType="abledPay" width={width} />
-        <CardSectionButton buttonType="abledPoint" width={width} />
+        <Link to="points/list">
+          <CardSectionButton buttonType="abledPoint" width={width} />
+        </Link>
+        {/* FIXME: 승인요청관리 페이지(G-10)으로 이동 */}
+        <CardSectionButton buttonType="management" width={width} />
 
         {/* FIXME: 추후 알람 개수에 따라 alert 개수 변경 */}
-        <ListButton width={width} alertCount={1}>
-          판매내역
-        </ListButton>
-        <ListButton width={width} alertCount={1}>
-          구매내역
-        </ListButton>
+        <Link to="/mypage/salesHistory">
+          <ListButton width={width} alertCount={1}>
+            판매내역
+          </ListButton>
+        </Link>
+
+        <Link to="/mypage/purchaseHistory">
+          <ListButton width={width} alertCount={1}>
+            구매내역
+          </ListButton>
+        </Link>
+
+        {/* FIXME: 야나바다 고객센터 페이지(G-7)으로 이동 */}
         <ListButton width={width}>야나바다 고객센터</ListButton>
+
+        {/* FIXME: 이용가이드 페이지(G-8)으로 이동 */}
         <ListButton width={width}>이용가이드</ListButton>
+
+        {/* FIXME: 이용약관 페이지(G-9)으로 이동 */}
         <ListButton width={width}>이용약관</ListButton>
-        <ListButton width={width}>로그아웃</ListButton>
+        <ListButton
+          width={width}
+          onClick={() => {
+            setIsLogoutModalVisible(true);
+          }}
+        >
+          로그아웃
+        </ListButton>
+        <Modal {...logoutModalProps} />
       </S.MyPageContainer>
     </>
   ) : (
@@ -48,13 +116,22 @@ const MyPage = ({ width }: MyPageProps) => {
       <UpperNavBar title="MY 야나바다" type="back" />
       <S.MyPageContainer>
         <S.LoginButtonWrapper>
-          <S.LoginButton>
-            로그인 및 회원가입 하기 <ArrowForwardIcon />
-          </S.LoginButton>
+          <Link to="/login">
+            <S.LoginButton>
+              로그인 및 회원가입 하기 <ArrowForwardIcon />
+            </S.LoginButton>
+          </Link>
         </S.LoginButtonWrapper>
 
-        <CardSectionButton buttonType="disabledPay" width="332px" />
-        <CardSectionButton buttonType="disabledPoint" width="332px" />
+        <CardSectionButton
+          buttonType="disabledPay"
+          width={width}
+          onClick={() => {
+            setIsLoginModalVisible(true);
+          }}
+        />
+        <Modal {...loginModalProps} />
+        <CardSectionButton buttonType="disabledPoint" width={width} />
 
         <ListButton width={width}>야나바다 고객센터</ListButton>
         <ListButton width={width}>이용가이드</ListButton>
