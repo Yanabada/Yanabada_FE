90681878dc870b2214c958b0c0f67000bd955164
wkdtnqls0506 <wkdtnqls0506@naver.com>
2024-01-19 17:51:40 +0900

    Feat: 카테고리 url params 설정

--- src/pages/products/components/CategoryTab/style.ts
+++ src/pages/products/components/CategoryTab/style.ts
@@ -7,16 +7,29 @@ export const CategoryContainer = styled.ul`
   gap: 1rem;
   padding: 0rem 1rem 0.75rem 1rem;
   border-bottom: 1px solid ${({ theme }) => theme.colors.gray[300]};
-
-  .selected_category {
-    color: ${({ theme }) => theme.colors.gray[900]};
-    ${({ theme }) => theme.text.button1};
-  }
 `;
 
 export const CategoryList = styled.li`
+  position: relative;
   color: ${({ theme }) => theme.colors.gray[600]};
   ${({ theme }) => theme.text.button2};
   white-space: nowrap;
   cursor: pointer;
+
+  &::before {
+    content: "";
+    position: absolute;
+    bottom: -65%;
+    width: 100%;
+    height: 0.1875rem;
+    transition: background 0.3s ease;
+  }
+
+  &.selected_category {
+    color: ${({ theme }) => theme.colors.gray[900]};
+    ${({ theme }) => theme.text.button1};
+    &::before {
+      background: ${({ theme }) => theme.colors.gray[900]};
+    }
+  }
 `;
