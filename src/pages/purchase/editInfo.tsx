0480d08682e8eb9fb6967b54a45570cf6c42a49b
고솔미 (Solmi Koh) <113079102+SOL-MI@users.noreply.github.com>
2024-01-20 17:43:54 +0900

    Fix : 빌드에러 및 디자인 수정

--- src/pages/purchase/editInfo.tsx
+++ src/pages/purchase/editInfo.tsx
@@ -1,7 +1,14 @@
 import Certification from "@components/certification";
 
 const EditInfo = () => {
-  return <Certification upperNavBarText="예약자 정보 수정" buttonText="확인" width="100%" />;
+  return (
+    <Certification
+      upperNavBarText="예약자 정보 수정"
+      buttonText="확인"
+      width="100%"
+      handleClick={() => {}}
+    />
+  );
 };
 
 export default EditInfo;
