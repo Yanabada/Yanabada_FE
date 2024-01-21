0dd8a4e91feb73e31eee0de6b84e14cd158c9591
고솔미 (Solmi Koh) <113079102+SOL-MI@users.noreply.github.com>
2024-01-21 17:36:53 +0900

    Feat: 이메일 검사 공통 컴포넌트로 분리

--- src/pages/signIn/components/signInFirst/SignInFirst.tsx
+++ src/pages/signIn/components/signInFirst/SignInFirst.tsx
@@ -1,7 +1,21 @@
 import CheckEmail from "@components/checkEmail";
+import { useSearchParams } from "react-router-dom";
 
 const SignInFirst = () => {
-  return <CheckEmail title="회원가입(1/4)" />;
+  const [searchParams] = useSearchParams();
+  const fromParams = searchParams.get("from");
+  const redirectParams = searchParams.get("redirect");
+
+  if (fromParams === "changePassword") {
+    return (
+      <CheckEmail
+        type="backClose"
+        title="비밀번호 재설정"
+        to={`/signin/1?from=changePassword&redirect=${redirectParams}`}
+      />
+    );
+  }
+  return <CheckEmail type="back" title="회원가입(1/4)" to="/signin/1" />;
 };
 
 export default SignInFirst;
