b234c14e22096e43b7018e57ce5637d88e9868db
고솔미 (Solmi Koh) <113079102+SOL-MI@users.noreply.github.com>
2024-01-21 18:15:50 +0900

    Fix: 빌드 에러 수정

--- src/pages/signIn/components/signInFourth/SignInFourth.tsx
+++ src/pages/signIn/components/signInFourth/SignInFourth.tsx
@@ -10,9 +10,6 @@ const SignInFourth = () => {
       width="100%"
       upperNavBarText={providerParams === "kakao" ? "회원가입(2/2)" : "회원가입(4/4)"}
       buttonText="회원가입 완료"
-      customHandleClick={() => {
-        console.log("test");
-      }}
       hasBottomSheet
       bottomSheetTitle="⠀"
       bottomSheetChildren="회원가입 완료 추카추카~~ 추카 이미지 추가추가"
