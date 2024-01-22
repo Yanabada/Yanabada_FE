0d7e870f2856355c7449f050a014e19d96afdb34
고솔미 (Solmi Koh) <113079102+SOL-MI@users.noreply.github.com>
2024-01-22 14:59:36 +0900

    Feat: instance 생성 및 수정

--- src/apis/instance.ts
+++ src/apis/instance.ts
@@ -1,5 +1,43 @@
-import axios from "axios";
+import axios, { AxiosInstance } from "axios";
+import { getAccessToken, refreshAccessToken } from "@utils/getCookie";
+import Cookies from "js-cookie";
 
-export const instance = axios.create({
-  baseURL: ""
+const instance: AxiosInstance = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+  timeout: 5000
 });
+
+instance.interceptors.request.use(
+  async (config) => {
+    config.headers["Content-Type"] = "application/json";
+    const accessToken = getAccessToken();
+    config.headers["Authorization"] = `Bearer ${accessToken}`;
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+instance.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    if (error.response?.status === 401) {
+      const originalRequest = error.config;
+
+      if (error.response.data.data.isNeededRefresh) {
+        const newAccessToken = await refreshAccessToken();
+        Cookies.set("accessToken", newAccessToken);
+
+        axios.defaults.headers["Authorization"] = `Bearer ${newAccessToken}`;
+        originalRequest.headers["Authorization"] = `Bearer ${newAccessToken}`;
+
+        return axios(originalRequest);
+      }
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default instance;
