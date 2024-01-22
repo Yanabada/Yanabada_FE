f93af47640aea434bff64f3bd4397b066c25de58
wkdtnqls0506 <wkdtnqls0506@naver.com>
2024-01-22 11:58:37 +0900

    Feat: 각 탭 쿼리스트링으로 변경

--- src/pages/search/components/KeywordTab.tsx
+++ src/pages/search/components/KeywordTab.tsx
@@ -1,12 +1,14 @@
 import SearchTab from "./SearchTab";
 import { useSearchTab } from "../stores/tabStore";
-import KeywordStore from "../stores/KeywordStore";
 import Keyword from "./SearchTab/Keyword";
 import SearchIcon from "@assets/icons/search_Search.svg?react";
+import { useSearchParams } from "react-router-dom";
 
 const KeywordTab = () => {
   const { tab, setTab } = useSearchTab();
-  const { locationName } = KeywordStore();
+
+  const [searchParams] = useSearchParams();
+  const keywordParams = searchParams.get("keyword");
 
   return (
     <SearchTab
@@ -14,7 +16,7 @@ const KeywordTab = () => {
       setIsOpen={() => setTab("keyword")}
       icon={<SearchIcon />}
       leftPlaceholder="키워드"
-      rightPlaceholder={locationName}
+      rightPlaceholder={keywordParams ?? "전체 지역"}
     >
       <Keyword />
     </SearchTab>
