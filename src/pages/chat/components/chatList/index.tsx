70ae34331ccf2640d4633ab9ad0f97c792c50b29
gaori <monooi@naver.com>
2024-01-23 03:22:43 +0900

    Feat: 채팅방 목록 API 연결

--- src/pages/chat/components/chatList/index.tsx
+++ src/pages/chat/components/chatList/index.tsx
@@ -1,29 +1,29 @@
 import { useNavigate } from "react-router-dom";
 import * as S from "./styles";
 import NumberBadge from "@components/numberBadge";
+import { ChatRoom } from "@pages/chat/types/chatRoom";
 
 interface ChatListProps {
-  roomId: number;
+  room: ChatRoom;
 }
-const ChatList = ({ roomId }: ChatListProps) => {
+const ChatList = ({ room }: ChatListProps) => {
   const navigate = useNavigate();
 
   return (
-    <S.List onClick={() => navigate(`/chat/${roomId}`)}>
-      <S.ProfileImg
-        src="https://i.pinimg.com/564x/4d/a5/dd/4da5dd3d7b0a981cd56ec1f7db32c0cf.jpg"
-        alt="프로필"
-      />
+    <S.List
+      onClick={() => navigate(`/chat?productId=${room.productId}&chatRoomCode=${room.productId}`)}
+    >
+      <S.ProfileImg src={room.partnerImage} alt="프로필" />
       <S.DetailContainer>
         <S.LeftContainer>
           <S.NameContainer>
-            <S.NickName>강쥐사랑해진짜로</S.NickName>
-            <S.RoomName>파라스파라 서울 더 그레이트 현대 런던 </S.RoomName>
+            <S.NickName>{room.partnerNickname}</S.NickName>
+            <S.RoomName>{room.productName} </S.RoomName>
           </S.NameContainer>
-          <S.ChatText>만원만 깎아줘만원만 깎아줘만원만 깎아줘만원만 깎아줘만원만 깎아줘</S.ChatText>
+          <S.ChatText>{room.lastChatMessage}</S.ChatText>
         </S.LeftContainer>
         <S.RightContainer>
-          <S.TimeText>오후5:50</S.TimeText>
+          <S.TimeText>{room.lastSentMessageTime.toISOString()}</S.TimeText>
           <NumberBadge number={3} isChatPage={true} />
         </S.RightContainer>
       </S.DetailContainer>
