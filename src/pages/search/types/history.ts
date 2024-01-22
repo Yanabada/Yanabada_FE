aebd82d6431b4e7acbbf7f514fb49113bba328ce
wkdtnqls0506 <wkdtnqls0506@naver.com>
2024-01-22 12:03:24 +0900

    Feat: history 전역변수 설정 및 타입 지정

---
+++ src/pages/search/types/history.ts
@@ -0,0 +1,8 @@
+export interface HistoryType {
+  id: Date;
+  keyword: string | null;
+  checkInDate: string | null;
+  checkOutDate: string | null;
+  adult: string | null;
+  child: string | null;
+}
