10ef528b0b4702aa5989e20c274140935d10d3ef
gaori <monooi@naver.com>
2024-01-23 03:20:47 +0900

    Feat: 채팅 API 함수 및 type 추가

--- src/pages/chat/types/chatRoom.ts
+++ src/pages/chat/types/chatRoom.ts
@@ -1,5 +1,40 @@
 export interface Message {
-  chatRoomCode: number;
+  sendDateTime: Date;
   senderId: number;
+  senderImage: string;
+  senderNickname: string;
   content?: string;
+}
+
+export interface MessageResponseData {
+  status: string;
+  data: {
+    chatRoomCode: number;
+    messages: Message[];
+    pageNum: number;
+    pageSize: number;
+    totalPages: number;
+    isFirst: boolean;
+    isLast: boolean;
+  };
+}
+
+export interface MessageRequestParam {
+  code: number;
+  page?: number;
+}
+
+export interface ChatRoom {
+  chatRoomCode: number;
+  partnerImage: string;
+  partnerNickname: string;
+  lastChatMessage: string;
+  lastSentMessageTime: Date;
+  productId: number;
+  productName: string;
+  unreadMessageCount: number;
+}
+export interface ChatRoomResponseData {
+  status: string;
+  data: ChatRoom[];
 }
