0c88e191e8bfd0fe12f4c0287c18d4e36f6991b6
wkdtnqls0506 <wkdtnqls0506@naver.com>
2024-01-24 01:43:35 +0900

    Feat: 야놀자 페이 상세 조회 API 생성 및 연결

--- src/pages/charge/index.tsx
+++ src/pages/charge/index.tsx
@@ -5,38 +5,48 @@ import { IoMdArrowDropup } from "react-icons/io";
 import { IoMdArrowDropdown } from "react-icons/io";
 import { GoCheck } from "react-icons/go";
 import { motion, AnimatePresence } from "framer-motion";
+import { Link } from "react-router-dom";
 
+import { numberFormat } from "@utils/numberFormat";
 import UpperNavBar from "@components/navBar/upperNavBar";
 import Notice from "@components/notice";
 import BaseButton from "@components/buttons/BaseButton";
 import PriceArea from "@components/priceArea";
+import usePaymentDetail from "./hooks/usePaymentDetail";
+import AmountStore from "./stores/amountStore";
 import YanoljaIcon from "@assets/icons/yanolja_Icon.svg?react";
 
 const Charge = () => {
   const [isOptionVisible, setIsOptionVisible] = useState(false);
   const [bankMessage, setBankMessage] = useState("계좌선택");
-  const [price, setPrice] = useState(0);
+  const { amount, setAmount } = AmountStore();
 
   const toggleOption = () => {
     setIsOptionVisible((prev) => !prev);
   };
 
+  const { data: paymentData, isLoading, error } = usePaymentDetail();
+
+  if (isLoading) return <p>Loading...</p>;
+
+  if (error) return <p>error</p>;
+
   return (
     <>
       <UpperNavBar title="충전" type="back" />
       <S.ChargeWrapper>
-        <S.PayBalance className="product-balance">
+        {/* <S.PayBalance className="product-balance">
           <div>
             <span className="text">₩ 상품가격</span>
           </div>
           <span className="price">623,000원</span>
-        </S.PayBalance>
+        </S.PayBalance> */}
         <S.PayBalance>
           <div>
             <YanoljaIcon />
             <span className="text">야놀자 페이 잔액</span>
           </div>
-          <span className="price">0원</span>
+          <span className="price">{numberFormat(paymentData.balance)}원</span>
         </S.PayBalance>
         <S.PaySpace />
         <S.PayTitle>페이를 충전하실 계좌를 선택해 주세요</S.PayTitle>
@@ -59,16 +69,16 @@ const Charge = () => {
                 initial={{ opacity: 0, y: -10 }}
                 animate={{ opacity: 1, y: 0 }}
                 onClick={() => {
-                  setBankMessage("국민은행 000000-00-000000");
+                  setBankMessage(`${paymentData.bankName} ${paymentData.accountNumber}`);
                   setIsOptionVisible(false);
                 }}
               >
                 <motion.div className="inner">
                   <motion.img src="/src/assets/bankIcon.png" />
-                  <span className="bank">국민은행</span>
+                  <span className="bank">{paymentData.bankName}</span>
                 </motion.div>
                 <motion.div className="inner">
-                  <div className="number">000000-00-000000</div>
+                  <div className="number">{paymentData.accountNumber}</div>
                   {bankMessage !== "계좌선택" && <GoCheck style={{ color: "#028161" }} />}
                 </motion.div>
               </motion.div>
@@ -83,20 +93,28 @@ const Charge = () => {
           resetPrice={0}
           isAlert={false}
           charge
-          price={price}
-          setPrice={setPrice}
+          price={amount}
+          setPrice={setAmount}
         />
-        <S.AlertBanner>
+        {/* <S.AlertBanner>
           <p className="text">
             야놀자페이 잔액이 부족합니다.
             <br />
             충전을 진행해주세요!
           </p>
-        </S.AlertBanner>
+        </S.AlertBanner> */}
         <S.ButtonWrapper>
-          <BaseButton buttonType="default" width="100%">
-            ₩ 충전하기
-          </BaseButton>
+          {amount < 10000 || amount > 2000000 ? (
+            <BaseButton buttonType="disabled-default" width="100%">
+              ₩ 충전하기
+            </BaseButton>
+          ) : (
+            <Link to="/charge/password?registration=false">
+              <BaseButton buttonType="default" width="100%">
+                ₩ 충전하기
+              </BaseButton>
+            </Link>
+          )}
         </S.ButtonWrapper>
       </S.ChargeWrapper>
     </>
