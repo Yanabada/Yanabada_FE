86be38019dd47acce697c90b10152d2fc381e035
Gaoridang <oingc@naver.com>
2024-01-24 23:47:01 +0900

    Fix: accumulate api 헤더 제거

--- src/pages/home/api/accumulate.ts
+++ src/pages/home/api/accumulate.ts
@@ -2,12 +2,7 @@ import axios from "axios";
 
 const getAccumulate = async () => {
   try {
-    const response = await axios.get("/payments/admin", {
-      headers: {
-        Authorization:
-          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJlbWFpbEBlbWFpbC5jb20iLCJyb2xlIjoiUk9MRV9VU0VSIiwicHJvdmlkZXIiOiJFTUFJTCIsImlhdCI6MTcwNTg0NDAxMiwiZXhwIjoxNzA2NDQ4ODEyfQ.1eLZM-jjI42emTMtZGGGkmqHOq3Y4fkq89MY6bUIebM"
-      }
-    });
+    const response = await axios.get("/payments/admin");
     return response.data.data;
   } catch (err) {
     console.error("getAccumulate: ", err);
