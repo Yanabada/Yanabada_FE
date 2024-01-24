659727246347f3d936a71f105f7298e84cf60c83
wkdtnqls0506 <wkdtnqls0506@naver.com>
2024-01-25 02:47:41 +0900

    Feat: 자신이 올린 상품일시 상품 정보 수정 버튼 노출

--- src/pages/productDetail/components/NegoButton/index.tsx
+++ src/pages/productDetail/components/NegoButton/index.tsx
@@ -9,6 +9,9 @@ interface DetailProps {
 }
 
 const NegoButton = ({ data }: DetailProps) => {
+  const member = localStorage.getItem("member");
+  const memberId = member ? JSON.parse(member).id : null;
+
   const navigate = useNavigate();
   return (
     <S.Container>
@@ -32,19 +35,32 @@ const NegoButton = ({ data }: DetailProps) => {
               <S.TimerText>{data.saleEndDate.toString()}</S.TimerText>
             </S.TimerContainer>
             <S.ButtonContainer>
-              <BaseButton
-                width="50%"
-                color={data.canNegotiate ? "#0751C3" : "#9C9C9C"}
-                backgroundColor={data.canNegotiate ? "#E6EEF9" : "#F2F2F2"}
-                buttonType="default"
-                children={data.canNegotiate ? "가격 제안하기" : "가격제안불가"}
-              />
-              <BaseButton
-                width="50%"
-                buttonType="default"
-                children="결제하기"
-                onClick={() => navigate(`/purchase?productId=${data.id}`)}
-              />
+              {memberId === data.seller.id ? (
+                <BaseButton
+                  width="100%"
+                  color="#fff"
+                  backgroundColor="#38A3EB"
+                  buttonType="default"
+                  children="상품 정보 수정"
+                  onClick={() => navigate(`/sell/register/correct/${data.id}?redirect=/detail`)}
+                />
+              ) : (
+                <>
+                  <BaseButton
+                    width="50%"
+                    color={data.canNegotiate ? "#0751C3" : "#9C9C9C"}
+                    backgroundColor={data.canNegotiate ? "#E6EEF9" : "#F2F2F2"}
+                    buttonType="default"
+                    children={data.canNegotiate ? "가격 제안하기" : "가격제안불가"}
+                  />
+                  <BaseButton
+                    width="50%"
+                    buttonType="default"
+                    children="결제하기"
+                    onClick={() => navigate(`/purchase?productId=${data.id}`)}
+                  />
+                </>
+              )}
             </S.ButtonContainer>
           </>
         )}
