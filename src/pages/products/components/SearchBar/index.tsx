869dbf6376708cccf9926f1282adcf512e9390a0
wkdtnqls0506 <wkdtnqls0506@naver.com>
2024-01-25 02:50:16 +0900

    Feat: 검색하기 버튼시 페이지 이동

--- src/pages/products/components/SearchBar/index.tsx
+++ src/pages/products/components/SearchBar/index.tsx
@@ -1,11 +1,26 @@
 import * as S from "./style";
+import { useLocation, useSearchParams } from "react-router-dom";
 
 const SearchBar = () => {
+  const { search } = useLocation();
+  const [searchParams] = useSearchParams();
+  const keyword = searchParams.get("keyword");
+  const adult = searchParams.get("adult");
+  const child = searchParams.get("child");
+  const checkInDate = searchParams.get("checkInDate")?.slice(5);
+  const checkOutDate = searchParams.get("checkOutDate")?.slice(5);
+
   return (
     <S.Container>
       <S.InputLabel>
         <S.InputIcon />
-        <S.Input placeholder="원하는 장소/날짜/ 인원을 검색해보세요" />
+        <S.Input
+          placeholder={
+            search
+              ? `${keyword} | ${checkInDate} ~ ${checkOutDate} | 성인 ${adult}, 아동 ${child}`
+              : "원하는 장소/날짜/인원을 검색해보세요"
+          }
+        />
       </S.InputLabel>
     </S.Container>
   );
