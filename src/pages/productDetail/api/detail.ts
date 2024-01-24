9253555064a30f091d1e069b0834be8171f166fb
Gaoridang <oingc@naver.com>
2024-01-24 18:19:13 +0900

    Fix: 에러 타입 수정

--- src/pages/productDetail/api/detail.ts
+++ src/pages/productDetail/api/detail.ts
@@ -1,4 +1,5 @@
 import instance from "@apis/instance";
+import { AxiosError } from "axios";
 import toast from "react-hot-toast";
 
 const getDetail = async (productId: number) => {
@@ -21,8 +22,7 @@ export const createChatRoom = async (productId: number, buyerId: number) => {
     return response.data;
   } catch (error) {
     console.error("Error", error);
-    if (error instanceof 
-        Error) {
+    if (error instanceof AxiosError) {
       if (
         error.response?.data.errorMessage ===
         ("네고 불가능한 상품입니다." || "이미 채팅방이 존재합니다.")
