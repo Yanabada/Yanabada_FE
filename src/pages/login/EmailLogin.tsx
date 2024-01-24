e34e01a32ab4cd4390f6f0ebde6a0b1d9dcc5b48
고솔미 (Solmi Koh) <113079102+SOL-MI@users.noreply.github.com>
2024-01-24 15:09:01 +0900

    Refactor: 로그인 api 호출 리팩토링

--- src/pages/login/EmailLogin.tsx
+++ src/pages/login/EmailLogin.tsx
@@ -10,6 +10,8 @@ import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 import { authInstance } from "@apis/instance";
 import Cookies from "js-cookie";
+import { Toaster } from "react-hot-toast";
+import useLogin from "./hooks/useLogin";
 
 const EmailLogin = () => {
   const navigate = useNavigate();
@@ -32,95 +34,84 @@ const EmailLogin = () => {
     trigger("password");
   }, []);
 
+  const { mutate } = useLogin();
   const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     if (errors.email || errors.password) {
       return;
     }
     const requestBody = { email, password };
-    try {
-      const res = await authInstance.post("/auth/login", requestBody);
-      // 로컬스토리지에 담는 용도 (백엔드분 코드 제대로 작동하는지 확인할 때 필요)
-      // localStorage.setItem("accessToken", res.data.data.tokenIssue.accessToken);
-      // localStorage.setItem("refreshToken", res.data.data.tokenIssue.refreshToken);
-
-      localStorage.setItem("member", JSON.stringify(res.data.data.member));
-      const accessTokenCookie = res.data.data.tokenIssue.accessToken;
-      const refreshTokenCookie = res.data.data.tokenIssue.refreshToken;
-
-      Cookies.set("accessToken", accessTokenCookie);
-      Cookies.set("refreshToken", refreshTokenCookie);
-
-      navigate("/");
-    } catch (error) {
-      console.error(error);
-    }
+    await mutate(requestBody);
   };
+  // const res = await authInstance.post("/auth/login", requestBody);
 
   return (
-    <S.EmailLoginContainer>
-      <UpperNavBar title="이메일로 로그인" type="back" shape="fill" />
-      <form onSubmit={onSubmit}>
-        <S.InputWrapper>
-          <div>
-            <TextInput
-              variant="move"
-              label="이메일"
-              isSuccess={true}
-              {...register("email", {
-                required: true,
-                pattern: {
-                  // 8-20자 사이, 최소 하나의 숫자, 영문자, 특수문자를 포함, 공백 X
-                  value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,}$/i,
-                  message: "유효한 이메일 주소를 입력하세요"
-                }
-              })}
-              errorMessage={errors.email && `${errors.email?.message}`}
-            />
-          </div>
-          <div>
-            <TextInput
-              variant="move"
-              label="비밀번호"
-              isSuccess={true}
-              {...register("password", {
-                required: true,
-                pattern: {
-                  value:
-                    /^(?=.*[A-Za-z])(?=.*\d)(?=.*[!@#$%^&*()-_+=])[A-Za-z\d!@#$%^&*()-_+=]{8,20}$/,
-                  message: "비밀번호가 올바르지 않습니다."
-                }
-              })}
-              errorMessage={errors.password && `${errors.password?.message}`}
-            />
-          </div>
-        </S.InputWrapper>
+    <>
+      <Toaster />
+      <S.EmailLoginContainer>
+        <UpperNavBar title="이메일로 로그인" type="back" shape="fill" />
+        <form onSubmit={onSubmit}>
+          <S.InputWrapper>
+            <div>
+              <TextInput
+                variant="move"
+                label="이메일"
+                isSuccess={!errors.email}
+                {...register("email", {
+                  required: true,
+                  pattern: {
+                    // 8-20자 사이, 최소 하나의 숫자, 영문자, 특수문자를 포함, 공백 X
+                    value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,}$/i,
+                    message: "유효한 이메일 주소를 입력하세요"
+                  }
+                })}
+                errorMessage={errors.email && `${errors.email?.message}`}
+              />
+            </div>
+            <div>
+              <TextInput
+                variant="move"
+                label="비밀번호"
+                isSuccess={!errors.password}
+                {...register("password", {
+                  required: true,
+                  pattern: {
+                    value:
+                      /^(?=.*[A-Za-z])(?=.*\d)(?=.*[!@#$%^&*()-_+=])[A-Za-z\d!@#$%^&*()-_+=]{8,20}$/,
+                    message: "비밀번호가 올바르지 않습니다."
+                  }
+                })}
+                errorMessage={errors.password && `${errors.password?.message}`}
+              />
+            </div>
+          </S.InputWrapper>
 
-        <BaseButton
-          buttonType={!errors.email && !errors.password ? "default" : "disabled-default"}
-          width="100%"
-          type="submit"
-        >
-          로그인
-        </BaseButton>
-      </form>
-      <S.BtnFlexContainer>
-        <C.BtnFlex
-          onClick={() => {
-            navigate("/signin?from=changePassword&redirect=/login/email");
-          }}
-        >
-          비밀번호 재설정
-          <IoChevronForwardSharp />
-        </C.BtnFlex>
-        |
-        <C.BtnFlex onClick={() => setSheetVisible(!sheetVisible)}>
-          이메일로 회원가입
-          <IoChevronForwardSharp />
-        </C.BtnFlex>
-      </S.BtnFlexContainer>
-      <SignInBottomSheet sheetVisible={sheetVisible} setSheetVisible={setSheetVisible} />
-    </S.EmailLoginContainer>
+          <BaseButton
+            buttonType={!errors.email && !errors.password ? "default" : "disabled-default"}
+            width="100%"
+            type="submit"
+          >
+            로그인
+          </BaseButton>
+        </form>
+        <S.BtnFlexContainer>
+          <C.BtnFlex
+            onClick={() => {
+              navigate("/signin?from=changePassword&redirect=/login/email");
+            }}
+          >
+            비밀번호 재설정
+            <IoChevronForwardSharp />
+          </C.BtnFlex>
+          |
+          <C.BtnFlex onClick={() => setSheetVisible(!sheetVisible)}>
+            이메일로 회원가입
+            <IoChevronForwardSharp />
+          </C.BtnFlex>
+        </S.BtnFlexContainer>
+        <SignInBottomSheet sheetVisible={sheetVisible} setSheetVisible={setSheetVisible} />
+      </S.EmailLoginContainer>
+    </>
   );
 };
 
