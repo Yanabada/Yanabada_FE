31075d9c22bd780f4c5b200bbf2b7e0590f8c397
고솔미 (Solmi Koh) <113079102+SOL-MI@users.noreply.github.com>
2024-01-25 14:18:37 +0900

    Refactor: 로그인 및 회원가입 auth instance로 연결

--- src/pages/signIn/apis/compareNicknameApi.ts
+++ src/pages/signIn/apis/compareNicknameApi.ts
@@ -1,8 +1,8 @@
-import instance from "@apis/instance";
+import { authInstance } from "@apis/instance";
 
 const compareNicknameApi = async (nickName: string): Promise<boolean> => {
   try {
-    const res = await instance.post("/auth/duplication/nickname", { nickName });
+    const res = await authInstance.post("/auth/duplication/nickname", { nickName });
     console.log(res);
     return res.data.data.isDuplication;
   } catch (error) {
