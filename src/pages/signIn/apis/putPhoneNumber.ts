1cda7e708647a21062054d7c6b10613d05c89b6f
seungsimdang <oak20005@naver.com>
2024-01-25 19:50:02 +0900

    Fix: 멤버 API URL 수정

--- src/pages/signIn/apis/putPhoneNumber.ts
+++ src/pages/signIn/apis/putPhoneNumber.ts
@@ -1,7 +1,7 @@
 import { instance } from "@apis/instance";
 
 export const putPhoneNumber = async (phoneNumber: string | null) => {
-  const putPhoneNumberURL = "/member/phone-number";
+  const putPhoneNumberURL = "/members/phone-number";
 
   return await instance.put(putPhoneNumberURL, {
     phoneNumber: phoneNumber
