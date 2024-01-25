ee74425517fa8087304f7955628c12a133e589b1
seungsimdang <oak20005@naver.com>
2024-01-25 18:37:29 +0900

    Fix: 토큰 요청 로직 수정

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -9,19 +9,40 @@ import CategoryList from "./components/CategoryList";
 import SubServices from "./components/SubServices";
 import Footer from "./components/Footer";
 import { Suspense } from "react";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import useFCMToken from "./hooks/useFCMToken";
 import { requestPermission } from "../../firebase-messaging-sw";
+import Cookie from "js-cookie";
 
 const Home = () => {
-  requestPermission();
-  const FCMToken = localStorage.getItem("FCMToken");
+  const [isLoginned, setIsLoginned] = useState(false);
+  const [FCMToken, setFCMToken] = useState<string | null | undefined>("");
+  const [tokenFromCookie, setTokenFromCookie] = useState<string | undefined>("");
 
   const { mutate } = useFCMToken();
 
   useEffect(() => {
-    if (FCMToken) {
+    const id = Cookie.get("id");
+    console.log("id", id);
+    if (id) {
+      setIsLoginned(true);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (isLoginned) {
+      requestPermission();
+      setFCMToken(localStorage.get("FCMToken"));
+    }
+  }, [isLoginned]);
+
+  const in60Minutes = 1 / 24;
+
+  useEffect(() => {
+    if (FCMToken && !tokenFromCookie) {
       mutate(FCMToken);
+      Cookie.set("FCMToken", FCMToken, { expires: in60Minutes });
+      setTokenFromCookie(Cookie.get("FCMToken"));
     }
   }, [FCMToken]);
 
