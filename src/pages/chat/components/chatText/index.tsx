41aa87b9772afb3cd5ba4dfe53bbd69d155d29ca
고솔미 (Solmi Koh) <113079102+SOL-MI@users.noreply.github.com>
2024-01-25 20:29:17 +0900

    Fix: 데이터 경로 수정

--- src/pages/chat/components/chatText/index.tsx
+++ src/pages/chat/components/chatText/index.tsx
@@ -1,15 +1,14 @@
 import * as S from "./styles";
 import * as CS from "../chatList/styles";
 import { Message } from "@pages/chat/types/chatRoom";
-
+import Cookies from "js-cookie";
 interface ChatTextProps {
   message: Message;
   isNotice?: boolean;
 }
 
 const ChatText = ({ message, isNotice = false }: ChatTextProps) => {
-  const userData = localStorage.getItem("member");
-  const userId = JSON.parse(userData!).id;
+  const userId = JSON.parse(Cookies.get("id")!);
   const isMe = message.senderId === userId;
 
   if (isNotice) {
