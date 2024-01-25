d9c175a4eed7ad9e4083312f577d4fc26cc13fcc
seungsimdang <oak20005@naver.com>
2024-01-25 16:45:07 +0900

    Fix: FCM 토큰 api 호출 url 수정

--- src/pages/home/api/putFCMToken.ts
+++ src/pages/home/api/putFCMToken.ts
@@ -3,7 +3,7 @@ import { instance } from "@apis/instance";
 export const putFCMToken = async (fcmToken: string) => {
   const putFCMTokenURL = `/members/fcm-token`;
 
-  const response = await instance.post(putFCMTokenURL, { fcmToken: fcmToken });
+  const response = await instance.put(putFCMTokenURL, { fcmToken: fcmToken });
 
   console.log(response.data.data);
 
