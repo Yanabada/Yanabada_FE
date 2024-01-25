b5a70511dc2f851fa5c07c2c325f0617f4474775
seungsimdang <oak20005@naver.com>
2024-01-25 14:08:17 +0900

    Chore: 줄바꿈 없애기

--- src/pages/productDetail/api/detail.ts
+++ src/pages/productDetail/api/detail.ts
@@ -3,7 +3,6 @@ import instance from "@apis/instance";
 const getDetail = async (productId: number) => {
   try {
     const response = await instance.get(`/products/${productId}`);
-
     return response.data.data;
   } catch (err) {
     console.error("getProducts: ", err);
