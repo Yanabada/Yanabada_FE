e532b0f82941250d187530c13e5409a1ad28b4fc
seungsimdang <oak20005@naver.com>
2024-01-25 19:37:44 +0900

    Fix: formatNumberWithCommas 함수 로직 수정

--- src/pages/myPage/utils/formatNumberWithCommas.ts
+++ src/pages/myPage/utils/formatNumberWithCommas.ts
@@ -1,5 +1,9 @@
 function formatNumberWithCommas(number: number | undefined): string | null {
-  if (!number) return null;
+  if (number === undefined || number === null) return null;
+
+  if (number < 100) {
+    return number.toString();
+  }
 
   return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
