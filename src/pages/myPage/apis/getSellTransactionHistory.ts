ca56525f218bfc51f0fa1237bb44a44c14a77511
seungsimdang <oak20005@naver.com>
2024-01-26 23:35:55 +0900

    Fix: Fixme 주석 로직 수정

--- src/pages/myPage/apis/getSellTransactionHistory.ts
+++ src/pages/myPage/apis/getSellTransactionHistory.ts
@@ -1,8 +1,6 @@
 import { instance } from "@apis/instance";
 
 const getSellTransactionHistory = async (tradeId: number | undefined) => {
-  // FIXME: 판매자가 /trades/approvals/${tradeId}에 접근할 수 없음
-  // const getSellTransactionHistoryURL = `/trades/purchases/${tradeId}`;
   const getSellTransactionHistoryURL = `/trades/approvals/${tradeId}`;
 
   const response = await instance.get(getSellTransactionHistoryURL);
--- src/pages/myPage/styles/pointsMiddleTab.styles.ts
+++ src/pages/myPage/styles/pointsMiddleTab.styles.ts
@@ -1,9 +1,5 @@
 import styled from "@emotion/styled";
-
-// FIXME: 모듈화
-interface PointsMiddleTabProps {
-  width?: string;
-}
+import PointsMiddleTabProps from "@pages/myPage/types/pointsMiddleTabProps";
 
 export const SectionDescriptionWrapper = styled.div<PointsMiddleTabProps>`
   display: flex;
@@ -137,54 +133,6 @@ export const NoItemText = styled.p`
 
   /* [body2]본문 */
   ${({ theme }) => theme.text.body2}
-`;
-
-export const PointsMiddleContainer = styled.div<PointsMiddleTabProps>`
-  display: flex;
-  width: ${({ width }) => width || "360px"};
-  justify-content: space-between;
-  align-items: flex-start;
-
-  margin-top: 20px;
-
-  border-bottom: 1px solid ${({ theme }) => theme.colors.gray[300]};
-`;
-
-export const MiddleWrapper = styled.button`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  gap: 10px;
-  flex: 1 0 0;
-`;
-
-export const MiddleTextWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  gap: 8px;
-`;
-
-export const UnderLine = styled.div`
-  height: 3px;
-  align-self: stretch;
-
-  background: ${({ theme }) => theme.colors.gray[900]};
-`;
-
-export const MiddleLeftText = styled.p`
-  color: ${({ theme }) => theme.colors.gray[900]};
-  text-align: center;
-
-  /* [Button1] 버튼진하게 */
-  ${({ theme }) => theme.text.button1}
-`;
-
-export const MiddleRightText = styled.p`
-  color: ${({ theme }) => theme.colors.gray[600]};
-
-  /* [Button2] 일반버튼 */
-  ${({ theme }) => theme.text.button2}
 `;
 
 export const DisappearMessageWrapper = styled.div`
