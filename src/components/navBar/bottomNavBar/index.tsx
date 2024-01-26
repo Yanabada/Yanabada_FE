bd8779ac9cef15dc197ea9dc18ca70de115fc1cf
고솔미 (Solmi Koh) <113079102+SOL-MI@users.noreply.github.com>
2024-01-26 23:45:27 +0900

    Feat: 채팅 안 읽은 알림 수 구현
    Co-authored-by: Lee JaeJun <monooi@naver.com>

--- src/components/navBar/bottomNavBar/index.tsx
+++ src/components/navBar/bottomNavBar/index.tsx
@@ -1,13 +1,18 @@
 import * as S from "./styles";
 import NavButton from "./NavButton";
+import useChatRooms from "@pages/chat/hooks/useChatRooms";
 
 const BottomNavBar = () => {
+  // 채팅 숫자 업데이트
+  const { data } = useChatRooms();
+  const chatCount = data?.data.reduce((acc, cur) => acc + cur.unreadMessageCount, 0);
+
   return (
     <S.BottomNavWrapper>
       <NavButton path="/" />
       <NavButton path="/products" />
       <NavButton path="/sell" />
-      <NavButton path="/chat" />
+      <NavButton path="/chat" chatCount={chatCount} />
       <NavButton path="/mypage" />
     </S.BottomNavWrapper>
   );
