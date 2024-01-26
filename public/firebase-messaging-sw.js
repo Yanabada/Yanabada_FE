bf5cb2030500ad7e2ac2700188ab8302b463da67
seungsimdang <oak20005@naver.com>
2024-01-27 00:53:19 +0900

    HotFix: FCM 테스트

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -12,15 +12,15 @@
 importScripts("https://www.gstatic.com/firebasejs/9.2.0/firebase-app-compat.js");
 importScripts("https://www.gstatic.com/firebasejs/9.2.0/firebase-messaging-compat.js");
 
-firebase.initializeApp({
-  apiKey: import.meta.env.VITE_APP_API_KEY,
-  authDomain: import.meta.env.VITE_APP_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_APP_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_APP_STORAGE_BUKET,
-  messagingSenderId: import.meta.env.VITE_APP_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_APP_APP_ID,
-  measurementId: import.meta.env.VITE_APP_MEASUREMENT_ID
-});
+// firebase.initializeApp({
+//   apiKey: import.meta.env.VITE_APP_API_KEY,
+//   authDomain: import.meta.env.VITE_APP_AUTH_DOMAIN,
+//   projectId: import.meta.env.VITE_APP_PROJECT_ID,
+//   storageBucket: import.meta.env.VITE_APP_STORAGE_BUKET,
+//   messagingSenderId: import.meta.env.VITE_APP_MESSAGING_SENDER_ID,
+//   appId: import.meta.env.VITE_APP_APP_ID,
+//   measurementId: import.meta.env.VITE_APP_MEASUREMENT_ID
+// });
 
 // Retrieve an instance of Firebase Messaging so that it can handle background
 // messages.
