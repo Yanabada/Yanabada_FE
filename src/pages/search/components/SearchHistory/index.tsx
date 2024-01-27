cc1b3ba0d5a29324edd2ad69afca6a726489eff7
wkdtnqls0506 <wkdtnqls0506@naver.com>
2024-01-28 02:44:44 +0900

    Fix: 검색 히스토리 수정

--- src/pages/search/components/SearchHistory/index.tsx
+++ src/pages/search/components/SearchHistory/index.tsx
@@ -2,12 +2,16 @@ import * as S from "./style";
 import HistoryStore from "@pages/search/stores/historyStore";
 import { HistoryType } from "@pages/search/types/history";
 import { formatDateTo } from "@utils/formatDateTo";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { useSearchParams } from "react-router-dom";
 
 const SearchHistory = () => {
+  const navigate = useNavigate();
   const { history, setHistory } = HistoryStore();
+  const [searchParams, setSearchParams] = useSearchParams();
 
-  const handleDeleteHistory = (id: Date) => {
+  const handleDeleteHistory = (id: Date, e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    e.stopPropagation();
     const deleteHistory = history.filter((item: HistoryType) => {
       return item.id != id;
     });
@@ -23,17 +27,33 @@ const SearchHistory = () => {
           checkInDate && checkOutDate ? `${checkInDate} ~ ${checkOutDate}` : "전체 기간";
         return (
           <S.RecordContainer key={item.id.toString()}>
-            <S.RecordBoxContainer>
-              <Link
-                to={`/products?keyword=${item.keyword}&checkInDate=${item.checkInDate}&checkOutDate=${item.checkOutDate}&adult=${item.adult}&child=${item.child}`}
-              >
-                <S.Location>{item.keyword ? item.keyword : "전체 지역"}</S.Location>
-                <S.Period>{checkDate}</S.Period>
-                <S.User>
-                  {item.adult ? item.adult : 2} 성인 / {item.child ? item.child : 0} 아동
-                </S.User>
-              </Link>
-              <S.CancelIcon onClick={() => handleDeleteHistory(item.id)} />
+            <S.RecordBoxContainer
+              onClick={() => {
+                if (item.keyword) {
+                  searchParams.set("keyword", item.keyword);
+                }
+                if (item.checkInDate) {
+                  searchParams.set("checkInDate", item.checkInDate);
+                }
+                if (item.checkOutDate) {
+                  searchParams.set("checkOutDate", item.checkOutDate);
+                }
+                if (item.adult) {
+                  searchParams.set("adult", item.adult);
+                }
+                if (item.child) {
+                  searchParams.set("child", item.child);
+                }
+                setSearchParams(searchParams);
+                navigate(`/products?${searchParams}`);
+              }}
+            >
+              <S.Location>{item.keyword ? item.keyword : "전체 지역"}</S.Location>
+              <S.Period>{checkDate}</S.Period>
+              <S.User>
+                {item.adult ? item.adult : 2} 성인 / {item.child ? item.child : 0} 아동
+              </S.User>
+              <S.CancelIcon onClick={(e) => handleDeleteHistory(item.id, e)} />
             </S.RecordBoxContainer>
           </S.RecordContainer>
         );
