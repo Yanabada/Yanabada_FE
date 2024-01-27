5a4e1a6708fd53140d1e62bfa06504709357db96
seungsimdang <oak20005@naver.com>
2024-01-27 18:17:02 +0900

    Design: 스크롤바 스타일링

--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -5,6 +5,18 @@ const globalStyles = css`
     margin: 0px;
     box-sizing: border-box;
   }
+
+  ::-webkit-scrollbar {
+    width: 8px;
+    height: 8px;
+    border-radius: 6px;
+    background: rgba(255, 255, 255, 0.4);
+  }
+  ::-webkit-scrollbar-thumb {
+    background: rgba(0, 0, 0, 0.3);
+    border-radius: 6px;
+  }
+
   a {
     text-decoration: none;
     color: inherit;
