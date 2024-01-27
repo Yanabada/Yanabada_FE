e64db4b68cbbdc841d20e9721b1a56c28ecbf5fd
seungsimdang <oak20005@naver.com>
2024-01-27 22:10:27 +0900

    Fix: 거래내역서 안보이는 현상 수정

--- src/pages/myPage/hooks/useSellTransactionHistory.ts
+++ src/pages/myPage/hooks/useSellTransactionHistory.ts
@@ -1,10 +1,11 @@
-import { useSuspenseQuery } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 import getSellTransactionHistory from "../apis/getSellTransactionHistory";
 
 const useSellTransactionHistory = (tradeId: number | undefined) => {
-  return useSuspenseQuery({
-    queryKey: ["sellTransactionHistory"],
-    queryFn: () => getSellTransactionHistory(tradeId)
+  return useQuery({
+    queryKey: ["sellTransactionHistory", tradeId],
+    queryFn: () => getSellTransactionHistory(tradeId),
+    enabled: false
   });
 };
 
