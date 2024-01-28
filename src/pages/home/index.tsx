7de15a7ab85061388547f563ef8d357e19e06779
wkdtnqls0506 <wkdtnqls0506@naver.com>
2024-01-28 17:55:58 +0900

    Feat: 홈 - 플로팅 버튼 추가

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -8,15 +8,18 @@ import SpecialServices from "./components/SpecialServices";
 import CategoryList from "./components/CategoryList";
 import SubServices from "./components/SubServices";
 import Footer from "./components/Footer";
+import FloatingAlarm from "./components/FloatingAlarm";
 import { Suspense } from "react";
 import { useEffect, useState } from "react";
 import useFCMToken from "./hooks/useFCMToken";
 import { requestPermission } from "../../firebase-messaging-sw";
 import Cookie from "js-cookie";
 import { getStoredToken } from "@utils/indexDB";
+import useNotifications from "@pages/notice/queries";
 
 const Home = () => {
   const isLoggedIn = Cookie.get("isLoggedIn") === "yes";
+  const { data } = useNotifications(isLoggedIn);
   const [FCMToken, setFCMToken] = useState<string | null | undefined>("");
   const [tokenFromCookie, setTokenFromCookie] = useState<string | undefined>("");
 
@@ -50,34 +53,39 @@ const Home = () => {
   }, [isSuccess]);
 
   return (
-    <S.Container>
-      <Header />
-      <SearchInput />
-      <Banner />
-      <RecentlyProduct />
-      <Suspense fallback={<p>Loading...</p>}>
-        <CategoryProduct
-          title="마감 임박 상품"
-          link="/products?order=END_DATE_ASC"
-          rowCardProps="마감임박"
-        />
-      </Suspense>
-      <Suspense fallback={<p>Loading...</p>}>
-        <CategoryProduct
-          title="높은 할인율 상품"
-          link="/products?order=SALES_PERCENTAGE_DESC"
-          rowCardProps="높은할인율"
-        />
-      </Suspense>
-      <Suspense fallback={<p>Loading...</p>}>
-        <SpecialServices />
-      </Suspense>
-      <CategoryList />
-      <Suspense fallback={<p>Loading...</p>}>
-        <SubServices />
-      </Suspense>
-      <Footer />
-    </S.Container>
+    <>
+      <S.Container>
+        <Header />
+        <SearchInput />
+        <Banner />
+        <RecentlyProduct />
+        <Suspense fallback={<p>Loading...</p>}>
+          <CategoryProduct
+            title="마감 임박 상품"
+            link="/products?order=END_DATE_ASC"
+            rowCardProps="마감임박"
+          />
+        </Suspense>
+        <Suspense fallback={<p>Loading...</p>}>
+          <CategoryProduct
+            title="높은 할인율 상품"
+            link="/products?order=SALES_PERCENTAGE_DESC"
+            rowCardProps="높은할인율"
+          />
+        </Suspense>
+        <Suspense fallback={<p>Loading...</p>}>
+          <SpecialServices />
+        </Suspense>
+        <CategoryList />
+        <Suspense fallback={<p>Loading...</p>}>
+          <SubServices />
+        </Suspense>
+        <Footer />
+      </S.Container>
+      {data?.data.notifications.map(
+        (item) => item.type === "TRADE_REQUEST" && <FloatingAlarm key={item.notificationId} />
+      )}
+    </>
   );
 };
 export default Home;
