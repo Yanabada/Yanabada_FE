0049353fc9dda55b0e7c988599228c69ff4e420c
wkdtnqls0506 <wkdtnqls0506@naver.com>
2024-01-28 12:33:44 +0900

    Feat: 최근 본 상품 연결

--- src/components/card/RecentlyRowCard/index.tsx
+++ src/components/card/RecentlyRowCard/index.tsx
@@ -1,50 +1,35 @@
 import * as S from "./styles";
 import { RiTimerLine } from "react-icons/ri";
+import RecentStore from "@pages/home/stores/recentStore";
+import { formatDateTo, formatRemainingTime } from "@utils/formatDateTo";
+import { Link } from "react-router-dom";
 
 const RecentlyRowCard = () => {
-  const products = [
-    {
-      id: 1,
-      image: "https://bit.ly/2Z4KKcF",
-      accommodationName: "파라스파라 서울 더 그레이트 현대 런던1",
-      roomName: "Forest Tower Deluxe King",
-      checkIn: "01/10",
-      checkOut: "01/13",
-      price: 1029000,
-      sellingPrice: 264000
-    },
-    {
-      id: 2,
-      image: "https://bit.ly/2Z4KKcF",
-      accommodationName: "파라스파라 서울 더 그레이트 현대 런던2",
-      roomName: "Forest Tower Deluxe King",
-      checkIn: "01/12",
-      checkOut: "01/13",
-      price: 2000000,
-      sellingPrice: 1500000
-    }
-  ];
+  const { recentItem } = RecentStore();
+
   return (
     <S.Wrapper>
-      {products.map((product) => (
-        <S.Container key={product.id}>
-          <S.ImageContainer>
-            <S.Image src="https://bit.ly/2Z4KKcF'" alt="숙소 이미지" />
-          </S.ImageContainer>
-          <S.InformationContainer>
-            <S.ProductContainer>
-              <S.ProductName>{product.accommodationName}</S.ProductName>
-              <S.RoomName>{product.roomName}</S.RoomName>
-              <S.Period>
-                {product.checkIn} ~ {product.checkOut}
-              </S.Period>
-            </S.ProductContainer>
-            <S.TimerContainer>
-              <RiTimerLine className="TimeIcon" />
-              <S.TimerText>3일 15시간 23분</S.TimerText>
-            </S.TimerContainer>
-          </S.InformationContainer>
-        </S.Container>
+      {recentItem.map((item) => (
+        <Link to={`/products/${item.id}`} key={item.id}>
+          <S.Container>
+            <S.ImageContainer>
+              <S.Image src={item.image} alt="숙소 이미지" />
+            </S.ImageContainer>
+            <S.InformationContainer>
+              <S.ProductContainer>
+                <S.ProductName>{item.accommodationName}</S.ProductName>
+                <S.RoomName>{item.roomName}</S.RoomName>
+                <S.Period>
+                  {formatDateTo(item.checkInDate)} ~ {formatDateTo(item.checkOutDate)}
+                </S.Period>
+              </S.ProductContainer>
+              <S.TimerContainer>
+                <RiTimerLine className="TimeIcon" />
+                <S.TimerText>{formatRemainingTime(item.saleEnd)}</S.TimerText>
+              </S.TimerContainer>
+            </S.InformationContainer>
+          </S.Container>
+        </Link>
       ))}
     </S.Wrapper>
   );
